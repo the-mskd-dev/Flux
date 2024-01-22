e66464a550aafa3bf1cc5f7d52815703f1ff5a39
kevyn <kevyn.buchet@rosemood.fr>
2024-01-22 15:27:13 +0100

    backup FluxMedia

--- app/src/main/java/com/kaem/flux/model/DataState.kt
+++ app/src/main/java/com/kaem/flux/model/DataState.kt
@@ -4,5 +4,4 @@ sealed  class DataState<T> {
     class Loading<T> : DataState<T>()
     data class Success<T>(val data: List<T>) : DataState<T>()
     data class Error<T>(val message: String) : DataState<T>()
-
 }
---
+++ app/src/main/java/com/kaem/flux/model/FluxMedia.kt
@@ -0,0 +1,50 @@
+package com.kaem.flux.model
+
+data class FluxMedia(
+    val name: String,
+    val season: Int?,
+    val episode: Float?,
+    val source: FluxSource
+) {
+
+    companion object {
+
+        fun parse(name: String) : Triple<String, Int?, Float?>? {
+
+            val splitFileName = name.split("_")
+
+            return when (splitFileName.size) {
+
+                1 -> Triple(splitFileName[0], null, null)
+
+                2 -> {
+
+                    val seasonAndEpisode = splitFileName[1]
+                        .lowercase()
+                        .removePrefix("s")
+                        .split("e")
+
+                    val season = seasonAndEpisode.getOrNull(0)?.toIntOrNull()
+                    val episode = seasonAndEpisode.getOrNull(1)?.toFloatOrNull()
+
+                    Triple(
+                        splitFileName[0],
+                        season,
+                        episode
+                    )
+
+                }
+
+                else -> null
+
+            }
+
+        }
+
+    }
+}
+
+sealed class FluxSource(val path: String) {
+    class Local(path: String) : FluxSource(path)
+    class GDrive(path: String) : FluxSource(path)
+}

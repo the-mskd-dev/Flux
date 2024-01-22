56f498e93ccae5319988f8a138391704ac4fd59f
kevyn <kevyn.buchet@rosemood.fr>
2024-01-22 11:30:05 +0100

    add permissions

--- app/src/main/java/com/kaem/flux/home/HomeScreen.kt
+++ app/src/main/java/com/kaem/flux/home/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.permissions.rememberPermissionState
 
 @Composable
 fun HomeScreen() {
@@ -97,6 +98,13 @@ fun HomeContent(
         }
 
     }
+
+}
+
+@Composable
+fun HomePermissionButton() {
+
+
 
 }
 

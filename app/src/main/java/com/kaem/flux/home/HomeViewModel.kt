b2d00a47be3c090d5a6ee1c46290cb5e18284639
kevyn <kevyn.buchet@rosemood.fr>
2024-01-22 10:37:22 +0100

    backup viewmodel

--- app/src/main/java/com/kaem/flux/home/HomeViewModel.kt
+++ app/src/main/java/com/kaem/flux/home/HomeViewModel.kt
@@ -1,6 +1,31 @@
 package com.kaem.flux.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+enum class HomeState {
+    Loading,
+    Content
+}
 
 class HomeViewModel : ViewModel() {
+
+    var state by mutableStateOf(HomeState.Loading)
+
+    init {
+
+        viewModelScope.launch {
+
+            delay(1000L)
+            state = HomeState.Content
+
+        }
+
+    }
+
 }

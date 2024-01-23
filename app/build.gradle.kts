f5d1ea317f3100d11a86de395c799a1133aeb45f
kevyn <kevyn.buchet@rosemood.fr>
2024-01-23 15:40:48 +0100

    backup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     testImplementation("junit:junit:4.13.2")

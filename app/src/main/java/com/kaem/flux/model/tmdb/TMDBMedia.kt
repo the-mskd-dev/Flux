7bb42c519b95e12c876d361330c83d1d6572627f
kevyn <kevyn.buchet@rosemood.fr>
2024-01-23 17:34:22 +0100

    serialized name

--- app/src/main/java/com/kaem/flux/model/tmdb/TMDBMedia.kt
+++ app/src/main/java/com/kaem/flux/model/tmdb/TMDBMedia.kt
@@ -6,8 +6,10 @@ data class TMDBMedia(
     val id: Int,
     val name: String,
     val overview: String,
-    val backdrop_path: String,
-    val poster_path: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
     @SerializedName("media_type")
     val type: TMDBMediaType,
     @SerializedName("genre_ids")
@@ -24,6 +26,8 @@ data class TMDBMedia(
 data class TMDBMediasResult(
     val page: Int,
     val results: List<TMDBMedia>,
-    val total_pages: Int,
-    val total_results: Int
+    @SerializedName("total_pages")
+    val pageCount: Int,
+    @SerializedName("total_results")
+    val resultCount: Int
 )

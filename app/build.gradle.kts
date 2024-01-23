5a49f56603754065f82b33595cbb6aef03c8982d
kevyn <kevyn.buchet@rosemood.fr>
2024-01-23 15:30:08 +0100

    add gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,12 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
 
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")

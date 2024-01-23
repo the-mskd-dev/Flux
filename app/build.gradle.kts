bf33ada59b222140b90de7a4e0cd7368e92f3c3b
kevyn <kevyn.buchet@rosemood.fr>
2024-01-23 15:00:29 +0100

    Android 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.kaem.flux"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

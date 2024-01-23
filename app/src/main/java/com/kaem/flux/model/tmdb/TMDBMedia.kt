69a8dde39d4e3b0fb7230c7128df86bf8638c6f3
kevyn <kevyn.buchet@rosemood.fr>
2024-01-23 17:29:52 +0100

    backup

--- app/src/main/java/com/kaem/flux/model/tmdb/TMDBMedia.kt
+++ app/src/main/java/com/kaem/flux/model/tmdb/TMDBMedia.kt
@@ -1,17 +1,24 @@
 package com.kaem.flux.model.tmdb
 
+import com.google.gson.annotations.SerializedName
+
 data class TMDBMedia(
-    val backdrop_path: String,
     val id: Int,
     val name: String,
     val overview: String,
+    val backdrop_path: String,
     val poster_path: String,
-    val media_type: TMDBMediaType,
-    val genre_ids: List<Int>,
+    @SerializedName("media_type")
+    val type: TMDBMediaType,
+    @SerializedName("genre_ids")
+    val genres: List<Int>,
     val popularity: Float,
-    val first_air_date: String,
-    val vote_average: Float,
-    val vote_count: Int
+    @SerializedName("first_air_date")
+    val releaseDateString: String,
+    @SerializedName("vote_average")
+    val voteAverage: Float,
+    @SerializedName("vote_count")
+    val voteCount: Int
 )
 
 data class TMDBMediasResult(

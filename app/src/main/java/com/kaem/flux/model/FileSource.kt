54c408e087a2da95d7668d7d5c5beeb799ab74ee
kevyn <kevyn.buchet@rosemood.fr>
2024-01-24 17:39:48 +0100

    FileNameProperties

--- app/src/main/java/com/kaem/flux/model/FileSource.kt
+++ app/src/main/java/com/kaem/flux/model/FileSource.kt
@@ -1,6 +1,7 @@
 package com.kaem.flux.model
 
 import android.net.Uri
+import java.util.regex.Pattern
 
 sealed class FileSource(
     val name: String,
@@ -10,4 +11,32 @@ sealed class FileSource(
     class Local(name: String, addedDateString: String, val uri: Uri) : FileSource(name, addedDateString)
     class GDrive(name: String, addedDateString: String, val path: String) : FileSource(name, addedDateString)
 
+    val nameProperties: FileNameProperties
+        get() {
+
+            val regex = Regex("""^(.+?)(?:\s*\((\d{4})\))?(?:_s(\d{1,2})e(\d{1,2}))?.*""")
+            val matchResult = regex.matchEntire(name)
+
+            return if (matchResult != null) {
+                val (name, year, season, episode) = matchResult.destructured
+                FileNameProperties(
+                    title = name.trim(),
+                    year = year.ifEmpty { null },
+                    season = season.toIntOrNull(),
+                    episode = episode.toFloatOrNull()
+                )
+            } else {
+                // If no match, return FileNameProperties with name only
+                FileNameProperties(title = name.trim())
+            }
+
+        }
+
 }
+
+data class FileNameProperties(
+    val title: String,
+    val year: String? = null,
+    val season: Int? = null,
+    val episode: Float? = null,
+)

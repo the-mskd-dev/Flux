a5fdc5ad9e5a93eb46521a6558bb20666d8491b6
kevyn <kevyn.buchet@rosemood.fr>
2024-01-24 12:10:13 +0100

    backup

--- app/src/main/java/com/kaem/flux/model/tmdb/TMDBMedia.kt
+++ app/src/main/java/com/kaem/flux/model/tmdb/TMDBMedia.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class TMDBMedia(
     val id: Int,
     val name: String,
-    val overview: String,
+    @SerializedName("overview")
+    val description: String,
     @SerializedName("poster_path")
     val posterPath: String,
     @SerializedName("backdrop_path")

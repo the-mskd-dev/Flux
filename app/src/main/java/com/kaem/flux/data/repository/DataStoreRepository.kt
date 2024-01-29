55ed17d257fc0a01e32114ac9ed7ea9e429ed2c1
kevyn <kevyn.buchet@rosemood.fr>
2024-01-29 11:58:01 +0100

    sort order

--- app/src/main/java/com/kaem/flux/data/repository/DataStoreRepository.kt
+++ app/src/main/java/com/kaem/flux/data/repository/DataStoreRepository.kt
@@ -4,6 +4,11 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import javax.inject.Inject
 
+enum class SortOrder {
+    NAME,
+    RELEASE_DATE,
+    ADDED_DATE
+}
 
 class DataStoreRepository @Inject constructor(private val dataStore: DataStore<Preferences>) {
 }

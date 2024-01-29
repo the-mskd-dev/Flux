a4ad5b08c112b3c4faeec6601e917901efad68ed
kevyn <kevyn.buchet@rosemood.fr>
2024-01-29 12:09:59 +0100

    DataStoreRepository

--- app/src/main/java/com/kaem/flux/data/repository/DataStoreRepository.kt
+++ app/src/main/java/com/kaem/flux/data/repository/DataStoreRepository.kt
@@ -3,6 +3,7 @@ package com.kaem.flux.data.repository
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -30,6 +31,12 @@ class DataStoreRepository @Inject constructor(private val dataStore: DataStore<P
         HomePreferences(
             sortOrder = SortOrder.entries[sortOrderOrdinal]
         )
+    }
+
+    suspend fun updateSortOrder(sortOrder: SortOrder) {
+        dataStore.edit {
+            it[PreferencesKeys.SORT_ORDER] = sortOrder.ordinal
+        }
     }
 
 }

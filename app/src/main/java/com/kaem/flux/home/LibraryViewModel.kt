0e5199d14d1540ff976ad67cfd94832ba04b56f2
kevyn <kevyn.buchet@rosemood.fr>
2024-01-29 14:44:53 +0100

    backup

--- app/src/main/java/com/kaem/flux/home/LibraryViewModel.kt
+++ app/src/main/java/com/kaem/flux/home/LibraryViewModel.kt
@@ -10,6 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,7 +31,11 @@ class LibraryViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(LibraryUiState())
     val uiState: StateFlow<LibraryUiState> = _uiState.asStateFlow()
 
-    init { refreshFiles() }
+    private val libraryPreferencesFlow = dataStoreRepository.preferencesFlow
+
+    init {
+        refreshFiles()
+    }
 
     fun refreshFiles() = viewModelScope.launch {
 

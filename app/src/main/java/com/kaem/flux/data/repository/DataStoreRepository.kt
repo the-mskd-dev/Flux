e3ece51ef631c9656bd2ef24d951bfb9ed539c5a
kevyn <kevyn.buchet@rosemood.fr>
2024-01-30 16:59:32 +0100

    last watched ids

--- app/src/main/java/com/kaem/flux/data/repository/DataStoreRepository.kt
+++ app/src/main/java/com/kaem/flux/data/repository/DataStoreRepository.kt
@@ -4,13 +4,16 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
 data class LibraryPreferences(
-    val sortOrder: SortOrder = SortOrder.RELEASE_DATE
+    val sortOrder: SortOrder = SortOrder.RELEASE_DATE,
+    val lastWatchedIds: List<Int> = listOf()
 )
 
 enum class SortOrder {
@@ -19,22 +22,51 @@ enum class SortOrder {
     ADDED_DATE
 }
 
-class DataStoreRepository @Inject constructor(private val dataStore: DataStore<Preferences>) {
+class DataStoreRepository @Inject constructor(
+    private val dataStore: DataStore<Preferences>,
+    private val gson: Gson
+) {
 
     private object PreferencesKeys {
         val SORT_ORDER = intPreferencesKey("sort_order")
+        val LAST_WATCHED_IDS = stringPreferencesKey("last_watched_ids")
     }
 
     val preferencesFlow: Flow<LibraryPreferences> = dataStore.data.map {
+
         val sortOrderOrdinal = it[PreferencesKeys.SORT_ORDER] ?: SortOrder.RELEASE_DATE.ordinal
+
+        val lastWatchedIdsString = it[PreferencesKeys.LAST_WATCHED_IDS] ?: ""
+        val lastWatchedIds = gson.fromJson<ArrayList<Int>>(lastWatchedIdsString, ArrayList::class.java)
+
         LibraryPreferences(
-            sortOrder = SortOrder.entries[sortOrderOrdinal]
+            sortOrder = SortOrder.entries[sortOrderOrdinal],
+            lastWatchedIds = lastWatchedIds
         )
     }
 
     suspend fun updateSortOrder(sortOrder: SortOrder) {
         dataStore.edit {
             it[PreferencesKeys.SORT_ORDER] = sortOrder.ordinal
+        }
+    }
+
+    suspend fun addWatchedArtwork(id: Int) {
+        dataStore.edit {
+
+            val lastWatchedIdsString = it[PreferencesKeys.LAST_WATCHED_IDS] ?: ""
+            val lastWatchedIds = gson.fromJson<ArrayList<Int>>(lastWatchedIdsString, ArrayList::class.java)
+
+            if (!lastWatchedIds.contains(id)) {
+
+                lastWatchedIds.add(0, id)
+
+                if (lastWatchedIds.size > 4)
+                    lastWatchedIds.removeLast()
+
+                it[PreferencesKeys.LAST_WATCHED_IDS] = gson.toJson(lastWatchedIds)
+            }
+
         }
     }
 

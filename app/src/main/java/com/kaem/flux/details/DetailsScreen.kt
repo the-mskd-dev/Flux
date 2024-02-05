cc57275e6f71909d50e6893167b06072e8e12616
kevyn <kevyn.buchet@rosemood.fr>
2024-02-05 15:46:38 +0100

    backup

--- app/src/main/java/com/kaem/flux/details/DetailsScreen.kt
+++ app/src/main/java/com/kaem/flux/details/DetailsScreen.kt
@@ -1,2 +1,25 @@
 package com.kaem.flux.details
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DetailsScreen() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = "DetailsScreen",
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+
+}

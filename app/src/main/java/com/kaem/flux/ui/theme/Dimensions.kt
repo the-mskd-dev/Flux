fa3ef1cb2b0505f453345b401b7a6bbce1e42a7b
kevyn <kevyn.buchet@rosemood.fr>
2024-02-14 14:52:50 +0100

    create dimensions

---
+++ app/src/main/java/com/kaem/flux/ui/theme/Dimensions.kt
@@ -0,0 +1,15 @@
+package com.kaem.flux.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object FluxFontSize {
+    val SMALL = 12.dp
+    val MEDIUM = 15.dp
+    val LARGE = 17.dp
+}
+
+object FluxSpace {
+    val SMALL = 8.dp
+    val MEDIUM = 16.dp
+    val LARGE = 24.dp
+}

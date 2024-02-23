34c61c24df83313dbd73c54b5643dd0430255718
kevyn <kevyn.buchet@rosemood.fr>
2024-02-23 14:59:59 +0100

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,18 +60,21 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     // Accompanist
     implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    // ExoPlayer
+    implementation("com.google.android.exoplayer:exoplayer:2.19.1")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.50")

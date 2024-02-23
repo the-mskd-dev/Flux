36566b288413046ff46dcc59a9da1e1a083a07b3
kevyn <kevyn.buchet@rosemood.fr>
2024-02-23 15:12:57 +0100

    backup video player

--- app/src/main/java/com/kaem/flux/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/kaem/flux/screens/player/PlayerScreen.kt
@@ -1,7 +1,26 @@
 package com.kaem.flux.screens.player
 
+import android.net.Uri
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
 import com.kaem.flux.screens.details.ArtworkViewModel
 
 @Composable
@@ -10,6 +29,48 @@ fun PlayerScreen(
     viewModel: PlayerViewModel = hiltViewModel()
 ) {
 
-    val filePath = viewModel.filePath
+    VideoPlayer(videoUri = Uri.parse(viewModel.filePath))
 
+}
+
+@Composable
+fun VideoPlayer(videoUri: Uri) {
+    val context = LocalContext.current
+    val exoPlayer = remember {
+        SimpleExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.fromUri(videoUri))
+            prepare()
+        }
+    }
+    
+    
+    val playbackState = exoPlayer.playbackState
+    val isPlaying = exoPlayer.isPlaying
+
+    AndroidView(
+        factory = { context ->
+            PlayerView(context).apply {
+                player = exoPlayer
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+
+    IconButton(
+        onClick = {
+            if (isPlaying) {
+                exoPlayer.pause()
+            } else {
+                exoPlayer.play()
+            }
+        },
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Icon(
+            imageVector = if (isPlaying) Icons.Filled.Warning else Icons.Filled.PlayArrow,
+            contentDescription = if (isPlaying) "Pause" else "Play",
+            tint = Color.White,
+            modifier = Modifier.size(48.dp)
+        )
+    }
 }

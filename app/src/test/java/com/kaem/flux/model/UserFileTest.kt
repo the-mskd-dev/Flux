f2b0cb4c327289cc8e2bbac9360b848b499a1808
kbuchet <kevyn.buchet@systeme-u.fr>
2024-11-29 15:01:57 +0100

    UserFileTest

---
+++ app/src/test/java/com/kaem/flux/model/UserFileTest.kt
@@ -0,0 +1,31 @@
+package com.kaem.flux.model
+
+import org.junit.Test
+
+
+class UserFileTest {
+
+    @Test
+    fun testParsingFileName() {
+
+        val files = listOf(
+            "Naruto_s02e09.mp4",
+            "Spider-man(2001).mp4"
+        )
+
+        val naruto = FileNameProperties.fromFileName(files[0])
+        val spiderman = FileNameProperties.fromFileName(files[1])
+
+        assert(naruto.title == "naruto")
+        assert(naruto.season == 2)
+        assert(naruto.episode == 9)
+        assert(naruto.year == null)
+
+        assert(spiderman.title == "spider-man(2001)")
+        assert(spiderman.season == null)
+        assert(spiderman.episode == null)
+        assert(spiderman.year == 2001)
+
+    }
+
+}

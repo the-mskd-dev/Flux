296dc626644c7ac640d9f2fc2f9f0fe071675152
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-06 12:40:55 +0100

    strings presentation

--- app/src/main/java/com/kaem/flux/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/kaem/flux/screens/welcome/WelcomeScreen.kt
@@ -289,16 +289,12 @@ data class Presentation(val title: String, val description: String)
 
 val presentations = listOf(
     Presentation(
-        title = "Titre 1",
-        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        title = "Bienvenue dans Flux",
+        description = "Retrouvez vos films, séries et animes, dans une librairie simple et organisée"
     ),
     Presentation(
-        title = "Titre 2",
-        description = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. "
-    ),
-    Presentation(
-        title = "Titre 3",
-        description = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,"
+        title = "Accès aux données",
+        description = "Pour le fonctionnement de l'application, nous avons besoin d'avoir accès à vos fichiers vidéos. Aucune donnée personnelle n'est récupérée ni transmise à quiconque"
     )
 )
 

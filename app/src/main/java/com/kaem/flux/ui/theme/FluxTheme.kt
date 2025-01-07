6bb26f7a28f6e3f5ff7042bc166ada78fb4dcfa7
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-07 12:58:49 +0100

    Dark theme

--- app/src/main/java/com/kaem/flux/ui/theme/FluxTheme.kt
+++ app/src/main/java/com/kaem/flux/ui/theme/FluxTheme.kt
@@ -17,16 +17,42 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val FluxColorScheme = darkColorScheme(
-    primary = Coral, // Most important, like buttons, titles, navigation bars...
-    onPrimary = Color.White,
-    primaryContainer = Coral,
-    onPrimaryContainer = Color.White,
-    secondary = LightCoral, // Less important, background, borders, texts
-    tertiary = ContrastCoral, // Additional color for contrast,
-    background = Color.Black,
-    surface = Color.Black,
-    onSurface = Color.White,
-    onBackground = Color.White,
+    primary = Color(0.9882353f, 0.6901961f, 0.8392157f),
+    onPrimary = Color(0.31764707f, 0.11372549f, 0.23529412f),
+    primaryContainer = Color(0.42352942f, 0.20392157f, 0.32941177f),
+    onPrimaryContainer = Color(1.0f, 0.84705883f, 0.9137255f),
+    inversePrimary = Color(0.5294118f, 0.29411766f, 0.42352942f),
+    secondary = Color(0.8745098f, 0.7411765f, 0.8f),
+    onSecondary = Color(0.2509804f, 0.16470589f, 0.20784314f),
+    secondaryContainer = Color(0.34509805f, 0.2509804f, 0.29803923f),
+    onSecondaryContainer = Color(0.99215686f, 0.8509804f, 0.9098039f),
+    tertiary = Color(0.9529412f, 0.7294118f, 0.60784316f),
+    onTertiary = Color(0.2901961f, 0.15686275f, 0.07058824f),
+    tertiaryContainer = Color(0.39215687f, 0.23921569f, 0.14901961f),
+    onTertiaryContainer = Color(1.0f, 0.85882354f, 0.7882353f),
+    background = Color(0.09411765f, 0.06666667f, 0.08235294f),
+    onBackground = Color(0.93333334f, 0.8745098f, 0.8901961f),
+    surface = Color(0.09411765f, 0.06666667f, 0.08235294f),
+    onSurface = Color(0.93333334f, 0.8745098f, 0.8901961f),
+    surfaceVariant = Color(0.3137255f, 0.2627451f, 0.28627452f),
+    onSurfaceVariant = Color(0.83137256f, 0.7607843f, 0.78431374f),
+    surfaceTint = Color(0.9882353f, 0.6901961f, 0.8392157f),
+    inverseSurface = Color(1.0f, 0.972549f, 0.972549f),
+    inverseOnSurface = Color(0.12941177f, 0.101960786f, 0.11372549f),
+    error = Color(0.9490196f, 0.72156864f, 0.70980394f),
+    onError = Color(0.3764706f, 0.078431375f, 0.0627451f),
+    errorContainer = Color(0.54901963f, 0.11372549f, 0.09411765f),
+    onErrorContainer = Color(0.9764706f, 0.87058824f, 0.8627451f),
+    outline = Color(0.6117647f, 0.5529412f, 0.5764706f),
+    outlineVariant = Color(0.3137255f, 0.2627451f, 0.28627452f),
+    scrim = Color(0.0f, 0.0f, 0.0f),
+    surfaceBright = Color(0.2509804f, 0.21568628f, 0.22745098f),
+    surfaceDim = Color(0.09411765f, 0.06666667f, 0.08235294f),
+    surfaceContainer = Color(0.14509805f, 0.11372549f, 0.12941177f),
+    surfaceContainerHigh = Color(0.1882353f, 0.15686275f, 0.16862746f),
+    surfaceContainerHighest = Color(0.23137255f, 0.19607843f, 0.21176471f),
+    surfaceContainerLow = Color(0.12941177f, 0.101960786f, 0.11372549f),
+    surfaceContainerLowest = Color(0.07450981f, 0.047058824f, 0.05882353f)
 )
 
 private val FluxLightColorScheme = lightColorScheme(
---
+++ app/src/main/java/com/kaem/flux/utils/extensions/ColorScheme+Extensions.kt
@@ -0,0 +1,53 @@
+package com.kaem.flux.utils.extensions
+
+import android.util.Log
+import androidx.compose.material3.ColorScheme
+import androidx.compose.ui.graphics.Color
+
+fun ColorScheme.logColorScheme() {
+
+    val colorScheme = this
+    fun getRGB(color: Color): Triple<Float, Float, Float> {
+        val red = color.red
+        val green = color.green
+        val blue = color.blue
+        return Triple(red, green, blue)
+    }
+
+    Log.i("ColorScheme", "primary : ${getRGB(colorScheme.primary)}")
+    Log.i("ColorScheme", "onPrimary : ${getRGB(colorScheme.onPrimary)}")
+    Log.i("ColorScheme", "primaryContainer : ${getRGB(colorScheme.primaryContainer)}")
+    Log.i("ColorScheme", "onPrimaryContainer : ${getRGB(colorScheme.onPrimaryContainer)}")
+    Log.i("ColorScheme", "inversePrimary : ${getRGB(colorScheme.inversePrimary)}")
+    Log.i("ColorScheme", "secondary : ${getRGB(colorScheme.secondary)}")
+    Log.i("ColorScheme", "onSecondary : ${getRGB(colorScheme.onSecondary)}")
+    Log.i("ColorScheme", "secondaryContainer : ${getRGB(colorScheme.secondaryContainer)}")
+    Log.i("ColorScheme", "onSecondaryContainer : ${getRGB(colorScheme.onSecondaryContainer)}")
+    Log.i("ColorScheme", "tertiary : ${getRGB(colorScheme.tertiary)}")
+    Log.i("ColorScheme", "onTertiary : ${getRGB(colorScheme.onTertiary)}")
+    Log.i("ColorScheme", "tertiaryContainer : ${getRGB(colorScheme.tertiaryContainer)}")
+    Log.i("ColorScheme", "onTertiaryContainer : ${getRGB(colorScheme.onTertiaryContainer)}")
+    Log.i("ColorScheme", "background : ${getRGB(colorScheme.background)}")
+    Log.i("ColorScheme", "onBackground : ${getRGB(colorScheme.onBackground)}")
+    Log.i("ColorScheme", "surface : ${getRGB(colorScheme.surface)}")
+    Log.i("ColorScheme", "onSurface : ${getRGB(colorScheme.onSurface)}")
+    Log.i("ColorScheme", "surfaceVariant : ${getRGB(colorScheme.surfaceVariant)}")
+    Log.i("ColorScheme", "onSurfaceVariant : ${getRGB(colorScheme.onSurfaceVariant)}")
+    Log.i("ColorScheme", "surfaceTint : ${getRGB(colorScheme.surfaceTint)}")
+    Log.i("ColorScheme", "inverseSurface : ${getRGB(colorScheme.inverseSurface)}")
+    Log.i("ColorScheme", "inverseOnSurface : ${getRGB(colorScheme.inverseOnSurface)}")
+    Log.i("ColorScheme", "error : ${getRGB(colorScheme.error)}")
+    Log.i("ColorScheme", "onError : ${getRGB(colorScheme.onError)}")
+    Log.i("ColorScheme", "errorContainer : ${getRGB(colorScheme.errorContainer)}")
+    Log.i("ColorScheme", "onErrorContainer : ${getRGB(colorScheme.onErrorContainer)}")
+    Log.i("ColorScheme", "outline : ${getRGB(colorScheme.outline)}")
+    Log.i("ColorScheme", "outlineVariant : ${getRGB(colorScheme.outlineVariant)}")
+    Log.i("ColorScheme", "scrim : ${getRGB(colorScheme.scrim)}")
+    Log.i("ColorScheme", "surfaceBright : ${getRGB(colorScheme.surfaceBright)}")
+    Log.i("ColorScheme", "surfaceDim : ${getRGB(colorScheme.surfaceDim)}")
+    Log.i("ColorScheme", "surfaceContainer : ${getRGB(colorScheme.surfaceContainer)}")
+    Log.i("ColorScheme", "surfaceContainerHigh : ${getRGB(colorScheme.surfaceContainerHigh)}")
+    Log.i("ColorScheme", "surfaceContainerHighest : ${getRGB(colorScheme.surfaceContainerHighest)}")
+    Log.i("ColorScheme", "surfaceContainerLow : ${getRGB(colorScheme.surfaceContainerLow)}")
+    Log.i("ColorScheme", "surfaceContainerLowest : ${getRGB(colorScheme.surfaceContainerLowest)}")
+}

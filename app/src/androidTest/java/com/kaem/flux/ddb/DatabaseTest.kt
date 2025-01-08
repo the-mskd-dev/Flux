35eda7246d0933f15cfe55cb559ff01406d60e80
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-08 13:08:08 +0100

    creation test

---
+++ app/src/androidTest/java/com/kaem/flux/ddb/DatabaseTest.kt
@@ -0,0 +1,10 @@
+package com.kaem.flux.ddb
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class DatabaseTest {
+}

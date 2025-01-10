70d6863e7b64c17e54682dddbd80fed1009a3407
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-10 13:58:59 +0100

    backup

--- app/src/test/java/com/kaem/flux/screens/home/HomeViewModelTest.kt
+++ app/src/test/java/com/kaem/flux/screens/home/HomeViewModelTest.kt
@@ -2,8 +2,10 @@ package com.kaem.flux.screens.home
 
 import app.cash.turbine.test
 import com.kaem.flux.data.repository.DataStoreRepository
+import com.kaem.flux.data.repository.FluxDataStore
 import com.kaem.flux.data.repository.LibraryContent
 import com.kaem.flux.data.repository.LibraryRepository
+import com.kaem.flux.mockups.ArtworkMockups
 import com.kaem.flux.model.ScreenState
 import com.kaem.flux.model.artwork.ArtworkOverview
 import io.mockk.coEvery
@@ -19,6 +21,7 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import java.util.prefs.Preferences
 
 class HomeViewModelTest {
 
@@ -52,6 +55,37 @@ class HomeViewModelTest {
             assert(emptyList<ArtworkOverview>() == initialState.overviews)
             assert(emptyList<Long>() == initialState.lastWatchedArtworkIds)
             assert(initialState.isSyncing)
+        }
+    }
+
+    @Test
+    fun combine_flows_should_update_state() = runTest {
+
+        // Mock
+        val overviews = listOf(ArtworkMockups.movieOverview, ArtworkMockups.showOverview)
+        val lastWatchedIds = listOf(ArtworkMockups.showOverview.id)
+        val libraryContent = mockk<LibraryContent> {
+            every { isLoading } returns false
+            every { artworkOverviews } returns overviews
+        }
+        val dataStore = mockk<FluxDataStore> {
+            every { lastWatchedIds } returns lastWatchedIds
+        }
+
+        // Mock flow
+        val mockedLibraryFlow = MutableStateFlow(libraryContent)
+        coEvery { repository.libraryFlow } returns mockedLibraryFlow
+        every { dataStoreRepository.flow } returns flowOf(dataStore)
+
+
+        viewModel.uiState.test {
+            val initialState = awaitItem() // Ignore initial state
+            val updatedState = awaitItem()
+
+            assert(ScreenState.CONTENT == updatedState.screenState)
+            assert(overviews == updatedState.overviews)
+            assert(lastWatchedIds == updatedState.lastWatchedArtworkIds)
+            assert(!updatedState.isSyncing)
         }
     }
 

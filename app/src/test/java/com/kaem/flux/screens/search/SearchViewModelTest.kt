09a54ad0c996a761cf383364ca9a7dcfcb9ef14d
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-13 12:19:59 +0100

    SearchViewModelTest

--- app/src/test/java/com/kaem/flux/screens/search/SearchViewModelTest.kt
+++ app/src/test/java/com/kaem/flux/screens/search/SearchViewModelTest.kt
@@ -7,9 +7,7 @@ import com.kaem.flux.data.repository.LibraryRepository
 import com.kaem.flux.mockups.ArtworkMockups
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -19,9 +17,9 @@ class SearchViewModelTest : BaseTest() {
     private lateinit var libraryRepository: LibraryRepository
 
     // Mocked data
-    private val libraryFlow = MutableStateFlow(LibraryContent(
-        artworkOverviews = ArtworkMockups.overviews
-    ))
+    private val libraryFlow = MutableStateFlow(
+        LibraryContent(artworkOverviews = ArtworkMockups.overviews)
+    )
 
     override fun setUp() {
         super.setUp()
@@ -30,41 +28,78 @@ class SearchViewModelTest : BaseTest() {
             every { libraryFlow } returns this@SearchViewModelTest.libraryFlow
         }
 
-
         viewModel = SearchViewModel(libraryRepository)
 
     }
 
     @Test
     fun `initial state`() = runTest {
+
         viewModel.uiState.test {
+
             val initialState = awaitItem()
 
             assert(initialState.searchWord == "")
             assert(initialState.overviews == ArtworkMockups.overviews)
 
-            cancelAndConsumeRemainingEvents()
         }
+
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `search word with result`() = runTest {
-
-        viewModel.updateSearchWord("nar")
-
-        advanceUntilIdle()
+    fun `search word with one result`() = runTest {
 
         viewModel.uiState.test {
+
             awaitItem()
+
+            viewModel.updateSearchWord("nar")
+
             val state = awaitItem()
 
             assert(state.searchWord == "nar")
             assert(state.filteredOverviews.size == 1)
             assert(state.filteredOverviews.any { it.title.equals("naruto", ignoreCase = true) })
 
-            cancelAndConsumeRemainingEvents()
         }
 
     }
+
+    @Test
+    fun `search word with multiple results`() = runTest {
+
+        viewModel.uiState.test {
+
+            awaitItem()
+
+            viewModel.updateSearchWord("na")
+
+            val state = awaitItem()
+
+            assert(state.searchWord == "na")
+            assert(state.filteredOverviews.size == 2)
+            assert(state.filteredOverviews == ArtworkMockups.overviews)
+
+        }
+
+    }
+
+    @Test
+    fun `search word with no result`() = runTest {
+
+        viewModel.uiState.test {
+
+            awaitItem()
+
+            viewModel.updateSearchWord("spider-man")
+
+            val state = awaitItem()
+
+            assert(state.searchWord == "spider-man")
+            assert(state.filteredOverviews.isEmpty())
+
+        }
+
+    }
+
 }

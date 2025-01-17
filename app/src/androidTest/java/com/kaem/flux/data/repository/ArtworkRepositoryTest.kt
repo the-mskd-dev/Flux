27d6920e5562e077ba8b430280c170a30b6bda8d
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-17 12:18:45 +0100

    ArtworkRepositoryTest

--- app/src/androidTest/java/com/kaem/flux/data/repository/ArtworkRepositoryTest.kt
+++ app/src/androidTest/java/com/kaem/flux/data/repository/ArtworkRepositoryTest.kt
@@ -5,9 +5,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.MediumTest
 import com.kaem.flux.data.ddb.FluxDao
 import com.kaem.flux.data.ddb.FluxDatabase
-import io.mockk.coVerify
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.advanceUntilIdle
+import com.kaem.flux.mockups.ArtworkMockups
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
@@ -22,7 +20,7 @@ class ArtworkRepositoryTest {
 
 
     @Before
-    fun setUpDatabase() {
+    fun setUpDatabase() = runTest {
 
         database = Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
@@ -33,6 +31,9 @@ class ArtworkRepositoryTest {
 
         repository = ArtworkRepository(db)
 
+        // Insert overviews
+        db.insertOverviews(listOf(ArtworkMockups.movieOverview, ArtworkMockups.showOverview))
+
     }
 
     @After
@@ -40,15 +41,74 @@ class ArtworkRepositoryTest {
         database.close()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
+
     @Test
-    fun get_artwork() = runTest {
+    fun get_artwork_movie() = runTest {
 
-        val content = repository.getArtwork(1L)
-        advanceUntilIdle()
+        // Given
+        db.insertMovies(listOf(ArtworkMockups.movie))
 
-        coVerify {
-            db.getOverview(any())
-        }
+        // When
+        val content = repository.getArtwork(ArtworkMockups.movieOverview.id)
+
+        // Then
+        assert(content.artworkOverview == ArtworkMockups.movieOverview)
+        assert(content.movie == ArtworkMockups.movie)
+    }
+
+    @Test
+    fun get_artwork_show() = runTest {
+
+        // Given
+        db.insertEpisodes(listOf(ArtworkMockups.episode1, ArtworkMockups.episode2))
+
+        // When
+        val content = repository.getArtwork(ArtworkMockups.showOverview.id)
+
+        // Then
+        assert(content.artworkOverview == ArtworkMockups.showOverview)
+        assert(content.episodes?.size == 2)
+    }
+
+    @Test
+    fun save_movie() = runTest {
+
+        // Given
+        val movie = ArtworkMockups.movie
+
+        // When
+        repository.saveMovie(movie)
+
+        // Then
+        val savedMovie = db.getMovie(movie.artworkId)
+        assert(movie == savedMovie)
+    }
+
+    @Test
+    fun save_episode() = runTest {
+
+        // Given
+        val episode = ArtworkMockups.episode1
+
+        // When
+        repository.saveEpisode(episode)
+
+        // Then
+        val savedEpisode = db.getEpisode(episode.id)
+        assert(episode == savedEpisode)
+    }
+
+    @Test
+    fun save_episodes() = runTest {
+
+        // Given
+        val episodes = listOf(ArtworkMockups.episode1, ArtworkMockups.episode2)
+
+        // When
+        repository.saveEpisodes(episodes)
+
+        // Then
+        val savedEpisodes = db.getEpisodes(episodes.first().artworkId)
+        assert(episodes == savedEpisodes)
     }
 }

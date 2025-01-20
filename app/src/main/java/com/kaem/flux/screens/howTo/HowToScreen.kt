d91e1dceab8c19738afd66a6f81a5a51212c8912
kbuchet <kevyn.buchet@systeme-u.fr>
2025-01-20 13:21:06 +0100

    backup texts

--- app/src/main/java/com/kaem/flux/screens/howTo/HowToScreen.kt
+++ app/src/main/java/com/kaem/flux/screens/howTo/HowToScreen.kt
@@ -1,6 +1,5 @@
 package com.kaem.flux.screens.howTo
 
-import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,14 +12,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.kaem.flux.R
 import com.kaem.flux.ui.component.BoldText
 import com.kaem.flux.ui.component.FluxTopBar
 import com.kaem.flux.ui.component.LightText
 import com.kaem.flux.ui.component.MediumText
-import com.kaem.flux.ui.theme.FluxTheme
 import com.kaem.flux.ui.theme.Ui
 
 @Composable
@@ -48,21 +44,21 @@ fun HowToScreen(onBackButtonTap: () -> Unit) {
             horizontalAlignment = Alignment.Start
         ) {
 
+            MediumText(
+                text = "Pas de panique, c’est très simple ! Pour que vos vidéos soient bien organisées, voici comment nommer vos fichiers :"
+            )
+
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(Ui.Space.MEDIUM),
             ) {
 
-                MediumText(
-                    text = "Pas de panique, c’est très simple ! Pour que vos vidéos soient bien organisées, voici comment nommer vos fichiers :"
-                )
-
                 BoldText(text = "Pour les films :")
                 MediumText(text = "Donnez à vos films un nom clair, suivi de l’année si vous le souhaitez. Par exemple :")
                 LightText(
-                    text = "Pulp Fiction (1994).mkv\n" +
-                            "Inception.avi\n" +
-                            "Captain-America-The-Winter-Soldier-(2014).mp4"
+                    text = "Spider-man (2002).mkv\n" +
+                            "Your name.avi\n" +
+                            "Spider-man-no-way-home-(2021).mp4"
                 )
 
             }
@@ -75,10 +71,10 @@ fun HowToScreen(onBackButtonTap: () -> Unit) {
                 BoldText(text = "Pour les séries :")
                 MediumText(text = "Pour les épisodes de séries, utilisez un format qui indique la saison et l’épisode. Par exemple :")
                 LightText(
-                    text = "show-name_s01.e02.mkv\n" +
-                            "show-name_1x02.mkv\n" +
-                            "show-name_se1.ep2.mkv\n" +
-                            "show-name-season1.episode2.mkv"
+                    text = "nom série_s01.e02.mkv\n" +
+                            "nom_série_1x02.mkv\n" +
+                            "nom_série_se1.ep2.mkv\n" +
+                            "nom_série-season1.episode2.mkv"
                 )
 
             }
@@ -90,8 +86,8 @@ fun HowToScreen(onBackButtonTap: () -> Unit) {
 
 }
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview
 @Composable
 fun HowToScreen_Preview() {
-    HowToScreen {  }
+    HowToScreen(onBackButtonTap = {})
 }

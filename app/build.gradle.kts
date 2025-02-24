1ea56eff920269557ed057247f7b6ee791ddf38b
Wahey <kevyn.buchet@gmail.com>
2025-02-24 12:38:26 +0100

    Crashlytics (#25)
    * dependencies
    
    * add crashlytics dependencies
    
    * add crashlytics dependencies
    
    ---------
    
    Co-authored-by: kbuchet <kevyn.buchet@systeme-u.fr>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("kotlin-parcelize")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -64,14 +65,14 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.0")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.7")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
 
     // Accompanist
@@ -116,8 +117,9 @@ dependencies {
     ksp("androidx.room:room-compiler:$roomVersion")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-crashlytics")
 
     implementation("app.cash.turbine:turbine:1.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
@@ -131,7 +133,7 @@ dependencies {
     androidTestImplementation("com.google.truth:truth:1.4.4")
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation("io.mockk:mockk-android:1.13.16")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")

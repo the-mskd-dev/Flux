3d34952c37bc7c4ea9df5d21de636d6b368da5ff
Wahey <kevyn.buchet@gmail.com>
2025-10-20 19:39:00 +0900

    use MaterialExpressiveTheme

--- app/src/main/java/com/kaem/flux/ui/theme/FluxTheme.kt
+++ app/src/main/java/com/kaem/flux/ui/theme/FluxTheme.kt
@@ -3,6 +3,8 @@ package com.kaem.flux.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -95,6 +97,7 @@ private val FluxLightColorScheme = lightColorScheme(
     surfaceContainerLowest = Color(1.0f, 1.0f, 1.0f), // #FFFFFF
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun FluxTheme(
     theme: Ui.THEME = Ui.THEME.SYSTEM,
@@ -133,7 +136,7 @@ fun FluxTheme(
 
     colorScheme.logDescription()
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content,

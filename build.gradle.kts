33ad44b5db47404b57a0c2fe0304f20d8d4c0348
Wahey <kevyn.buchet@gmail.com>
2025-11-27 14:10:54 +0900

    version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" // this version matches with the Kotlin version
-    id("com.google.dagger.hilt.android") version "2.57.2" apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
-    id("com.google.firebase.crashlytics") version "3.0.6" apply false
+
+    // Android
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+
+    // Kotlin
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.parcelize) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+
+    // Hilt
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+
+    // Google & Firebase
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
 }

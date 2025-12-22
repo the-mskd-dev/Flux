7f5095be91b60eb01de51f1760fc5e366fe10016
Wahey <kevyn.buchet@gmail.com>
2025-12-22 12:39:42 +0900

    PlayerSeekBar

--- app/src/main/java/com/kaem/flux/screens/player/composables/PlayerSeekBar.kt
+++ app/src/main/java/com/kaem/flux/screens/player/composables/PlayerSeekBar.kt
@@ -3,6 +3,7 @@ package com.kaem.flux.screens.player.composables
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,8 +17,7 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.LinearWavyProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
-import androidx.compose.material3.WavyProgressIndicatorDefaults
-import androidx.compose.material3.rememberSliderState
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -30,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.media3.exoplayer.ExoPlayer
 import com.kaem.flux.model.artwork.Media
@@ -50,13 +51,11 @@ fun PlayerSeekBar(
 
     var sliderPosition by rememberSaveable { mutableFloatStateOf(exoPlayer.currentPosition.toFloat()) }
     val interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
-    val isPressed by interactionSource.collectIsPressedAsState()
+    val isDragged by interactionSource.collectIsDraggedAsState()
 
-    LaunchedEffect(state.showInterface) {
-        while (state.showInterface) {
-            if (exoPlayer.isPlaying && !isPressed) {
-                sliderPosition = exoPlayer.currentPosition.coerceAtLeast(0L).toFloat()
-            }
+    LaunchedEffect(state.showInterface, isDragged) {
+        while (state.showInterface && !isDragged) {
+            sliderPosition = exoPlayer.currentPosition.coerceAtLeast(0L).toFloat()
             delay(200)
         }
     }
@@ -80,6 +79,13 @@ fun PlayerSeekBar(
                 stopSize = 10.dp
             )
 
+        },
+        thumb = {
+
+            SliderDefaults.Thumb(
+                interactionSource = interactionSource,
+                thumbSize = DpSize(4.dp, 22.dp)
+            )
         }
     )
 

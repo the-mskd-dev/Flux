77c4355773bae439b6d3fc81fb991e6fab08825f
Wahey <kevyn.buchet@gmail.com>
2025-12-26 18:26:46 +0900

    new

--- app/src/main/java/com/kaem/flux/utils/extensions/List+Extensions.kt
+++ app/src/main/java/com/kaem/flux/utils/extensions/List+Extensions.kt
@@ -9,3 +9,33 @@ fun List<Episode>.getPreviousEpisodesFor(episode: Episode) : List<Episode> {
 }
 
 val List<Episode>.lastEpisode get() = this.maxWith(compareBy<Episode> { it.season }.thenBy { it.number })
+
+fun List<Episode>.getPreviousEpisodeFor(episode: Episode) : Episode? {
+    return this
+        .filter { it.season == episode.season }
+        .sortedBy { it.number }
+        .let {
+
+            val index = it.indexOf(episode)
+
+            when {
+                index < 0 || index == 0 -> null
+                else -> it[index - 1]
+            }
+        }
+}
+
+fun List<Episode>.getNextEpisodeFor(episode: Episode) : Episode? {
+    return this
+        .filter { it.season == episode.season }
+        .sortedBy { it.number }
+        .let {
+
+            val index = it.indexOf(episode)
+
+            when {
+                index < 0 || index == it.lastIndex -> null
+                else -> it[index + 1]
+            }
+    }
+}

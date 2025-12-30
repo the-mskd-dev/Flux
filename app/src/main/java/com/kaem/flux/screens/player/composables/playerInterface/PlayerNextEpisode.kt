a3d102c83d6f246bd910a35f6cad1adf0d02c053
Wahey <kevyn.buchet@gmail.com>
2025-12-30 16:06:05 +0900

    PlayerNextEpisode

--- app/src/main/java/com/kaem/flux/screens/player/composables/playerInterface/PlayerNextEpisode.kt
+++ app/src/main/java/com/kaem/flux/screens/player/composables/playerInterface/PlayerNextEpisode.kt
@@ -1,26 +1,51 @@
 package com.kaem.flux.screens.player.composables.playerInterface
 
+import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kaem.flux.R
 import com.kaem.flux.mockups.MediaMockups
 import com.kaem.flux.model.artwork.Episode
 import com.kaem.flux.screens.player.PlayerIntent
@@ -28,7 +53,7 @@ import com.kaem.flux.ui.component.Image
 import com.kaem.flux.ui.component.Text
 import com.kaem.flux.ui.theme.AppTheme
 import com.kaem.flux.ui.theme.Ui
-import com.kaem.flux.utils.extensions.tmdbImage
+import kotlinx.coroutines.delay
 
 @Composable
 fun PlayerNextEpisode(
@@ -46,34 +71,62 @@ fun PlayerNextEpisode(
 
         episode ?: return@AnimatedVisibility
 
-        Surface(
-            color = MaterialTheme.colorScheme.scrim,
-            contentColor = Color.White,
-            shape = Ui.Shape.Corner.ExtraSmall,
-            onClick = { sendIntent(PlayerIntent.PlayNextEpisode(episode = episode)) }
-        ) {
+        var progress by remember { mutableFloatStateOf(0f) }
+        val animatedProgress by animateFloatAsState(
+            targetValue = progress.coerceIn(0f, 1f),
+            label = "BackgroundProgress",
+            animationSpec = tween(
+                durationMillis = 550,
+                easing = LinearEasing
+            )
+        )
 
-            Column(
-                modifier = Modifier.padding(Ui.Space.EXTRA_SMALL),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(Ui.Space.EXTRA_SMALL)
-            ) {
-
-                Image(
-                    modifier = Modifier
-                        .width(100.dp)
-                        .aspectRatio(16f / 9f),
-                    url = episode.imagePath.tmdbImage,
-                    contentDescription = "Season ${episode.season} episode ${episode.number}, ${episode.title}"
-                )
-
-                Text.Label.Small("Next episode")
-
+        LaunchedEffect(Unit) {
+            while (progress < 1f) {
+                progress += 0.1f
+                delay(500L)
             }
-
         }
 
+        ProgressiveText(progress = animatedProgress)
+
     }
+
+}
+
+@Composable
+fun ProgressiveText(
+    progress: Float,
+    backgroundColor: Color = MaterialTheme.colorScheme.tertiaryContainer,
+    progressColor: Color = MaterialTheme.colorScheme.tertiary,
+    textColor: Color = MaterialTheme.colorScheme.onTertiary
+) {
+
+    val density = LocalDensity.current
+    val layoutDirection = LocalLayoutDirection.current
+
+    Text.Label.Large(
+        modifier = Modifier
+            .clip(Ui.Shape.Corner.Medium)
+            .background(backgroundColor)
+            .drawBehind {
+                val width = size.width * progress
+
+                val outline = RectangleShape.createOutline(
+                    size = size.copy(width = width),
+                    layoutDirection = layoutDirection,
+                    density = density
+                )
+
+                drawOutline(
+                    outline = outline,
+                    color = progressColor
+                )
+            }
+            .padding(Ui.Space.MEDIUM),
+        text = stringResource(R.string.next_episode).uppercase(),
+        color = textColor
+    )
 
 }
 
@@ -81,10 +134,8 @@ fun PlayerNextEpisode(
 @Composable
 fun PlayerNextEpisode_Preview() {
     AppTheme {
-        PlayerNextEpisode(
-            modifier = Modifier,
-            episode = MediaMockups.episode1,
-            sendIntent = {}
-        )
+        Column {
+            ProgressiveText(progress = .4f)
+        }
     }
 }

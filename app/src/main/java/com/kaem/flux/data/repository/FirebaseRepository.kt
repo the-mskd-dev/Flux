a501358a2d53bb85fb8fbd477ead46e691685c48
Wahey <kevyn.buchet@gmail.com>
2026-02-21 16:51:55 +0900

    Create Message

--- app/src/main/java/com/kaem/flux/data/repository/FirebaseRepository.kt
+++ app/src/main/java/com/kaem/flux/data/repository/FirebaseRepository.kt
@@ -1,10 +1,44 @@
 package com.kaem.flux.data.repository
 
+import android.util.Log
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
+import com.google.gson.Gson
 import javax.inject.Inject
 
 class FirebaseRepository @Inject constructor(
-    private val remoteConfig: FirebaseRemoteConfig
+    private val remoteConfig: FirebaseRemoteConfig,
+    private val gson: Gson
 ) {
+
+    init {
+        initRemoteConfig()
+        testRemoteConfig()
+    }
+
+    private fun initRemoteConfig() {
+
+        Log.d("FirebaseRepository", "initRemoteConfig")
+
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+    }
+
+    private fun testRemoteConfig() {
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val result = remoteConfig.getString("test")
+                    Log.d("FirebaseRepository", "testRemoteConfig: $result")
+                } else {
+                    Log.e("FirebaseRepository", "testRemoteConfig: fail to get value")
+                }
+            }
+    }
 
 }
---
+++ app/src/main/java/com/kaem/flux/model/remoteConfig/Message.kt
@@ -0,0 +1,8 @@
+package com.kaem.flux.model.remoteConfig
+
+data class Message(
+    val versionCode: Int,
+    val title: String,
+    val subtitle: String,
+    val message: String,
+)

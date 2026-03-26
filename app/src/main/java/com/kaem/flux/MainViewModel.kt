72c99ce0a71c4b68d16be18ab09084be3e0769b6
Wahey <kevyn.buchet@gmail.com>
2026-03-26 09:38:36 +0900

    backup

--- app/src/main/java/com/kaem/flux/MainViewModel.kt
+++ app/src/main/java/com/kaem/flux/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.kaem.flux
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kaem.flux.data.repository.settings.SettingsRepository
+import com.kaem.flux.data.tmdb.token.TokenProvider
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -11,6 +12,7 @@ import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     private val settingsRepository: SettingsRepository,
+    private val tokenProvider: TokenProvider
 ) : ViewModel() {
 
     private val _settings = MutableStateFlow(SettingsRepository.State())
@@ -23,6 +25,7 @@ class MainViewModel @Inject constructor(
                 _settings.update { preferences }
             }
         }
+
     }
 
 }

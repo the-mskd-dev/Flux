f7aea70fbef40479a5786d586bb51d7db4bef3a7
the masked dev <the.masked.dev@proton.me>
2026-04-07 20:20:58 +0900

    Version 1.0.2 (4) (#91)
    * [FIX] Fastlane metadata (#84)
    
    * fix fastlane metadata
    
    * fix fastlane metadata
    
    * [FIX] F-droid (#86)
    
    * fix fastlane metadata
    
    * fix fastlane metadata
    
    * remove useless properties
    
    * add ndkVersion
    
    * [CI] Try to homogenize F-Droid and Github Actions (#89)
    
    * update github actions to java 21
    
    * enable minify
    
    * changelog
    
    * create beta release
    
    * beta ressources
    
    * lines
    
    * [UPDATE] Version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ configure<ApplicationExtension> {
         applicationId = "com.mskd.flux"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

313891a073c672f3a4e5c65af4cc390c61960a69
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-04-10 10:43:22 +0900

    backup UnknownScreen

--- app/src/main/java/com/mskd/flux/screens/unknown/UnknownScreen.kt
+++ app/src/main/java/com/mskd/flux/screens/unknown/UnknownScreen.kt
@@ -1,14 +1,66 @@
 package com.mskd.flux.screens.unknown
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.mskd.flux.R
+import com.mskd.flux.model.ScreenState
+import com.mskd.flux.model.artwork.Episode
 import com.mskd.flux.navigation.Route
+import com.mskd.flux.screens.artwork.ArtworkIntent
+import com.mskd.flux.ui.component.ErrorScreen
+import com.mskd.flux.ui.component.LoadingScreen
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun UnknownScreen(
     navigate: (Route) -> Unit,
-    onBack: () -> Unit
+    onBack: () -> Unit,
+    viewModel: UnknownViewModel = hiltViewModel()
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.event.collect { event ->
+            when (event) {
+                UnknownEvent.BackToPreviousScreen -> onBack()
+                is UnknownEvent.PlayMedia -> navigate(Route.Player(mediaId = event.mediaId))
+            }
+        }
+    }
+
+    Crossfade(targetState = uiState.screen) { screen ->
+
+        when (screen) {
+            ScreenState.LOADING -> LoadingScreen()
+            ScreenState.ERROR -> {
+                ErrorScreen(
+                    message = stringResource(R.string.oups_an_error_occured),
+                    onBackButtonTap = { viewModel.handleIntent(UnknownIntent.OnBackTap) }
+                )
+            }
+            ScreenState.CONTENT -> {
+                UnknownScreenContent(
+                    medias = uiState.medias,
+                    sendIntent = viewModel::handleIntent
+                )
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun UnknownScreenContent(
+    medias: List<Episode>,
+    sendIntent: (UnknownIntent) -> Unit
 ) {
 
 }
--- app/src/main/java/com/mskd/flux/screens/unknown/UnknownViewModel.kt
+++ app/src/main/java/com/mskd/flux/screens/unknown/UnknownViewModel.kt
@@ -1,11 +1,24 @@
 package com.mskd.flux.screens.unknown
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mskd.flux.data.repository.artwork.ArtworkRepository
 import com.mskd.flux.data.repository.user.UserRepository
+import com.mskd.flux.model.ScreenState
+import com.mskd.flux.model.artwork.Artwork
+import com.mskd.flux.model.artwork.Media
+import com.mskd.flux.screens.artwork.ArtworkEvent
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +26,44 @@ class UnknownViewModel @Inject constructor(
     private val repository: ArtworkRepository,
     private val userRepository: UserRepository
 ) : ViewModel() {
+
+    //region Flow
+
+    private val _event = MutableSharedFlow<UnknownEvent>()
+    val event = _event.asSharedFlow().distinctUntilChanged()
+
+    val uiState: StateFlow<UnknownUiState> = repository.flow
+        .map {
+            UnknownUiState(
+                screen = ScreenState.CONTENT,
+                medias = it.episodes
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = UnknownUiState()
+        )
+
+    //endregion
+
+    //region Init
+
+    init {
+        repository.searchArtwork(artworkId = Artwork.UNKNOWN_ID)
+    }
+
+    //endregion
+
+    //region Public Methods
+
+    fun handleIntent(intent: UnknownIntent) = viewModelScope.launch {
+        when (intent) {
+            is UnknownIntent.PlayMedia -> _event.emit(UnknownEvent.PlayMedia(mediaId = intent.media.mediaId))
+            UnknownIntent.OnBackTap -> _event.emit(UnknownEvent.BackToPreviousScreen)
+        }
+    }
+
+    //endregion
+
 }

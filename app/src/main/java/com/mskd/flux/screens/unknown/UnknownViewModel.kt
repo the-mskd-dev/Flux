5538e3d42efb02825f740cc67a61b8b2536113d1
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-04-10 09:36:11 +0900

    backup UnknownViewModel

--- app/src/main/java/com/mskd/flux/screens/unknown/UnknownViewModel.kt
+++ app/src/main/java/com/mskd/flux/screens/unknown/UnknownViewModel.kt
@@ -1,4 +1,16 @@
 package com.mskd.flux.screens.unknown
 
-class UnknownViewModel {
+import androidx.lifecycle.ViewModel
+import com.mskd.flux.data.repository.artwork.ArtworkRepository
+import com.mskd.flux.data.repository.user.UserRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UnknownViewModel @Inject constructor(
+    private val repository: ArtworkRepository,
+    private val userRepository: UserRepository
+) : ViewModel() {
 }

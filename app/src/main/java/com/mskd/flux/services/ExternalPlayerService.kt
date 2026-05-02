a1f2e7b1f7a047a5b77aae8186d197b42803a11d
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-02 09:09:47 +0900

    backup

--- app/src/main/java/com/mskd/flux/services/ExternalPlayerService.kt
+++ app/src/main/java/com/mskd/flux/services/ExternalPlayerService.kt
@@ -9,9 +9,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.ServiceInfo
 import android.os.Build
-import android.service.notification.NotificationListenerService
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.mskd.flux.R
@@ -21,7 +19,7 @@ class ExternalPlayerService : Service() {
     companion object {
         private const val ACTION_STOP = "StopExternalPlayerService"
         private const val NOTIFICATION_CHANNEL_ID = "ExternalPlayerService"
-        private const val NOTIFICATION_ID = 42
+        private const val NOTIFICATION_ID = 616
 
         fun start(context: Context) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
@@ -34,7 +32,7 @@ class ExternalPlayerService : Service() {
         }
     }
 
-    private val stopReceiver = object : BroadcastReceiver() {
+    private val stopServiceBroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (intent.action == ACTION_STOP) stopSelf()
         }
@@ -42,16 +40,15 @@ class ExternalPlayerService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val intentFilter = IntentFilter(ACTION_STOP)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            registerReceiver(stopReceiver, IntentFilter(ACTION_STOP), RECEIVER_NOT_EXPORTED)
+            registerReceiver(stopServiceBroadcastReceiver, intentFilter, RECEIVER_NOT_EXPORTED)
         } else {
-            ContextCompat.registerReceiver(
-                this,
-                stopReceiver,
-                IntentFilter(ACTION_STOP),
-                ContextCompat.RECEIVER_NOT_EXPORTED
-            )
+            ContextCompat.registerReceiver(this, stopServiceBroadcastReceiver, intentFilter, ContextCompat.RECEIVER_NOT_EXPORTED)
         }
+        
         startForeground(NOTIFICATION_ID, createNotification())
     }
 
@@ -59,7 +56,7 @@ class ExternalPlayerService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(stopReceiver)
+        unregisterReceiver(stopServiceBroadcastReceiver)
     }
 
     override fun onBind(intent: Intent?) = null
@@ -69,7 +66,8 @@ class ExternalPlayerService : Service() {
         createNotificationChannel(notificationManager)
 
         val stopIntent = PendingIntent.getBroadcast(
-            this, 0,
+            this,
+            0,
             Intent(ACTION_STOP).apply { setPackage(packageName) },
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
@@ -82,14 +80,14 @@ class ExternalPlayerService : Service() {
             .setContentText(getString(R.string.external_player_service_text))
             .setWhen(0)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .addAction(0, "Stop", stopIntent)
+            .addAction(0, getString(R.string.stop), stopIntent)
             .build()
     }
 
     private fun createNotificationChannel(manager: NotificationManager) {
         val channel = NotificationChannel(
             NOTIFICATION_CHANNEL_ID,
-            NOTIFICATION_CHANNEL_ID,
+            getString(R.string.app_name),
             NotificationManager.IMPORTANCE_LOW
         ).apply {
             enableVibration(false)

c7c1a22f65e1bc8e4e5b02c373a081c921f73f0e
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-04 11:19:49 +0900

    [UPDATE] Version 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ configure<ApplicationExtension> {
         applicationId = "com.mskd.flux"
         minSdk = 29
         targetSdk = 37
-        versionCode = 14
-        versionName = "1.3.1"
+        versionCode = 15
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }

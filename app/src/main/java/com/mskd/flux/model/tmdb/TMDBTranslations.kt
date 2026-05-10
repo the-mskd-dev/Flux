b1d24939b67042cf2d169aaa045336a84ac8449f
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-10 19:48:04 +0900

    filter language methods

--- app/src/main/java/com/mskd/flux/model/tmdb/TMDBTranslations.kt
+++ app/src/main/java/com/mskd/flux/model/tmdb/TMDBTranslations.kt
@@ -1,6 +1,7 @@
 package com.mskd.flux.model.tmdb
 
 import com.google.gson.annotations.SerializedName
+import com.mskd.flux.ui.typography.titleLarge
 import java.util.Locale
 
 data class TMDBTranslations(
@@ -11,6 +12,8 @@ data class TMDBTranslations(
     /**
      * Represents a translation for a show or a movie.
      *
+     * @property language Language in ISO-639-1
+     * @property country Country in ISO-3166-1
      * @property name Name of the language in the original language
      * @property englishName Name of the language in english
      * @property data Name and overview in the designed language
@@ -18,6 +21,8 @@ data class TMDBTranslations(
     data class Translation(
         @SerializedName("iso_639_1")
         val language: String,
+        @SerializedName("iso_3166_1")
+        val country: String,
         val name: String,
         @SerializedName("english_name")
         val englishName: String,
@@ -32,9 +37,11 @@ data class TMDBTranslations(
 }
 
 fun Collection<TMDBTranslations.Translation>.findEnglish() : TMDBTranslations.Translation? {
-    return this.find { it.language == Locale.ENGLISH.language }
+    return this.find { it.language == Locale.ENGLISH.language && it.data.overview.isNotBlank() }
 }
 
 fun Collection<TMDBTranslations.Translation>.findWithLocale(locale: Locale) : TMDBTranslations.Translation? {
-    return this.find { it.language == locale.language }
+    return this.find { it.language == locale.language && it.country == locale.country && it.data.overview.isNotBlank() }
+        ?: this.find { it.language == locale.language && it.data.overview.isNotBlank() }
+        ?: this.findEnglish()
 }

fcb8f3cfa3c9f01ffd25d08b583833ccaff2757c
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-10 17:30:34 +0900

    TMDBTranslation

--- app/src/main/java/com/mskd/flux/model/tmdb/TMDBTranslation.kt
+++ app/src/main/java/com/mskd/flux/model/tmdb/TMDBTranslation.kt
@@ -2,6 +2,13 @@ package com.mskd.flux.model.tmdb
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Represents a translation for a show or a movie.
+ *
+ * @property name Name of the language in the original language
+ * @property englishName Name of the language in english
+ * @property data Name and overview in the designed language
+ */
 data class TMDBTranslation(
     val name: String,
     @SerializedName("english_name")

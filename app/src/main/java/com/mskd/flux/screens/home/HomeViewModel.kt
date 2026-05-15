679f995dde82a90cc189114b4ea70624c17e2328
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-15 22:26:45 +0900

    HomeViewModelTest

--- app/src/main/java/com/mskd/flux/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/mskd/flux/screens/home/HomeViewModel.kt
@@ -7,6 +7,7 @@ import com.mskd.flux.BuildConfig
 import com.mskd.flux.data.repository.snackbars.SnackbarRepository
 import com.mskd.flux.data.repository.user.UserRepository
 import com.mskd.flux.data.tmdb.token.TokenRepository
+import com.mskd.flux.model.AppInfo
 import com.mskd.flux.model.ScreenState
 import com.mskd.flux.model.artwork.Artwork
 import com.mskd.flux.screens.home.HomeEvent.NavigateToArtwork
@@ -32,7 +33,8 @@ class HomeViewModel @Inject constructor(
     private val catalogUC: CatalogUC,
     private val userRepository: UserRepository,
     private val tokenRepository: TokenRepository,
-    private val snackbarRepository: SnackbarRepository
+    private val snackbarRepository: SnackbarRepository,
+    private val appInfo: AppInfo
 ): ViewModel() {
 
     private val _event = MutableSharedFlow<HomeEvent>()
@@ -101,7 +103,7 @@ class HomeViewModel @Inject constructor(
         val currentTime = System.currentTimeMillis()
         val sync = currentTime - lastSyncTime > 1.days.inWholeMilliseconds
                 || manualSync
-                || lastSyncVersionCode < BuildConfig.VERSION_CODE
+                || lastSyncVersionCode < appInfo.versionCode
 
         if (sync) {
 

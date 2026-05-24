917f202ce6a3593bf368fc8c17f3527e74a06ab6
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-24 10:27:28 +0900

    backup

--- app/src/main/java/com/mskd/flux/screens/customization/CustomizationContract.kt
+++ app/src/main/java/com/mskd/flux/screens/customization/CustomizationContract.kt
@@ -1,8 +1,13 @@
 package com.mskd.flux.screens.customization
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+import com.mskd.flux.R
+import com.mskd.flux.screens.settings.SettingsIntent
+import com.mskd.flux.ui.component.FluxOptionsDialogItem
 import com.mskd.flux.ui.component.FluxOptionsDialogState
 import com.mskd.flux.ui.theme.Ui
+import java.util.Locale
 
 @Immutable
 data class CustomizationUiState(
@@ -10,4 +15,21 @@ data class CustomizationUiState(
     val dialogState: FluxOptionsDialogState<*, CustomizationIntent>? = null,
 )
 
-sealed class CustomizationIntent
+sealed class CustomizationIntent {
+    data class SetThemeValue(val color: Color) : CustomizationIntent()
+}
+
+object CustomizationOptionsDialogs {
+
+    fun theme(currentValue: Color) = FluxOptionsDialogState(
+        titleResId = R.string.app_theme,
+        currentValue = currentValue,
+        options = listOf(
+            FluxOptionsDialogItem(value = Color.Red, labelResId = Ui.THEME.LIGHT.stringResourceId),
+            FluxOptionsDialogItem(value = Color.Blue, labelResId = Ui.THEME.DARK.stringResourceId),
+            FluxOptionsDialogItem(value = Color.Green, labelResId = Ui.THEME.SYSTEM.stringResourceId)
+        ),
+        applyValue = { value -> CustomizationIntent.SetThemeValue(value) }
+    )
+
+}

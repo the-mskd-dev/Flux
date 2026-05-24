ad54137911c906c09c514ee86dccbfadb5beaea6
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-05-24 18:42:51 +0900

    LoadingScreen text color

--- app/src/main/java/com/mskd/flux/ui/component/LoadingScreen.kt
+++ app/src/main/java/com/mskd/flux/ui/component/LoadingScreen.kt
@@ -1,11 +1,13 @@
 package com.mskd.flux.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularWavyProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,7 +21,9 @@ fun LoadingScreen(
 ) {
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
 
@@ -34,7 +38,10 @@ fun LoadingScreen(
                 CircularWavyProgressIndicator()
             }
 
-            Text.Title.Medium(text)
+            Text.Title.Medium(
+                text = text,
+                color = MaterialTheme.colorScheme.onBackground
+            )
 
         }
 

e52397173052c9d97484df3f03de79075e177a89
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-06-03 08:58:04 +0900

    FluxImage in ArtworkImage

--- app/src/main/java/com/mskd/flux/screens/artwork/composables/common/ArtworkImage.kt
+++ app/src/main/java/com/mskd/flux/screens/artwork/composables/common/ArtworkImage.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -14,23 +12,17 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalContext
-import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
 import com.mskd.flux.mockups.MediaMockups
 import com.mskd.flux.model.artwork.Episode
 import com.mskd.flux.model.artwork.FullArtwork
 import com.mskd.flux.model.artwork.Media
-import com.mskd.flux.ui.component.Image
+import com.mskd.flux.ui.component.FluxImage
 import com.mskd.flux.ui.theme.AppTheme
 import com.mskd.flux.utils.FluxPreview
-import com.mskd.flux.utils.extensions.tmdbImage
-import com.mskd.flux.utils.extensions.tmdbImageLarge
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,34 +35,23 @@ fun ArtworkImage(
 
     val episode = currentMedia as? Episode
     val imagePath = episode?.imagePath ?: fullArtwork.artwork.bannerPath
-
-    val imageUrl = imagePath.tmdbImageLarge
-    val placeHolderUrl = imageUrl.tmdbImage
-
     var imageSize by remember { mutableIntStateOf(0) }
-    val imageRequest = ImageRequest.Builder(LocalContext.current)
-        .data(imageUrl)
-        .crossfade(true)
-        .placeholderMemoryCacheKey(placeHolderUrl)
-        .build()
 
     Box(modifier = modifier.onSizeChanged { imageSize = if (orientation == Orientation.Vertical) it.height else it.width }) {
 
-        AsyncImage(
-            modifier = Modifier.fillMaxWidth(),
-            model = imageRequest,
-            contentScale = ContentScale.Crop,
-            placeholder = Image.placeholder,
-            error = Image.error,
-            alpha = .8f,
+        FluxImage(
+            modifier = Modifier
+                .matchParentSize()
+                .alpha(.8f),
+            path = imagePath,
+            hd = true,
             contentDescription = "background ${fullArtwork.artwork.title}"
         )
 
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .matchParentSize()
                 .then(
-
                     if (orientation == Orientation.Vertical) {
                         Modifier.background(
                             brush = Brush.verticalGradient(
@@ -90,7 +71,6 @@ fun ArtworkImage(
                     } else {
                         Modifier
                     }
-
                 )
         )
 

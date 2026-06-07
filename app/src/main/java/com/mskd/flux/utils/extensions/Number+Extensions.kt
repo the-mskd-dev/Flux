b32b639aceb58483b7acacbfc9430a6e1cf1a56b
the masked dev <the.masked.dev@proton.me>
2026-06-07 21:38:27 +0900

    [UI] Add rating in episodes (#246)

--- app/src/main/java/com/mskd/flux/utils/extensions/Number+Extensions.kt
+++ app/src/main/java/com/mskd/flux/utils/extensions/Number+Extensions.kt
@@ -38,4 +38,4 @@ fun Long.formatMinSec(): String {
 val Long.msToMin : Long get() = this.milliseconds.inWholeMinutes
 val Int.minToMs : Long get() = this.minutes.inWholeMilliseconds
 
-val Float.toRating : String get() = "%.2f".format(this).trimEnd('0').trimEnd('.')
+val Float.toRating : String get() = if (this <= 0f) "-" else "%.2f".format(this).trimEnd('0').trimEnd('.')

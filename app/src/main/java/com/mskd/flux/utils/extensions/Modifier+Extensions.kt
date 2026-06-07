f74583ada2c9c752ba08498789cc1e9c132f33bc
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-06-07 13:54:14 +0900

    clickableWithBounce

--- app/src/main/java/com/mskd/flux/utils/extensions/Modifier+Extensions.kt
+++ app/src/main/java/com/mskd/flux/utils/extensions/Modifier+Extensions.kt
@@ -1,5 +1,14 @@
 package com.mskd.flux.utils.extensions
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
@@ -8,6 +17,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.graphicsLayer
 
 fun Modifier.grayScale() : Modifier {
     val saturationMatrix = ColorMatrix().apply { setToSaturation(0f) }
@@ -24,4 +34,26 @@ fun Modifier.grayScale() : Modifier {
             }
         }
     }
+}
+
+@Composable
+fun Modifier.clickableWithBounce(
+    onClick: () -> Unit
+): Modifier {
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val animatedScale by animateFloatAsState(
+        targetValue = if (isPressed) .92f else 1f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessMedium
+        ),
+        label = "press_scale"
+    )
+
+    return this
+        .graphicsLayer { scaleX = animatedScale; scaleY = animatedScale }
+        .clickable(interactionSource = interactionSource, indication = null, onClick = onClick)
 }

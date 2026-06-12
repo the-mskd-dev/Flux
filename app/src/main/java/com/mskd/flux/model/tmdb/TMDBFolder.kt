20db9339ca897e9b1f869260981c7e75f70931d1
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-06-12 22:36:28 +0900

    Serializable TMDBFolder

--- app/src/main/java/com/mskd/flux/model/tmdb/TMDBFolder.kt
+++ app/src/main/java/com/mskd/flux/model/tmdb/TMDBFolder.kt
@@ -1,7 +1,9 @@
 package com.mskd.flux.model.tmdb
 
 import com.mskd.flux.model.UserFile
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class TMDBFolder(
     val tmdbArtwork: TMDBArtwork,
     val files: List<UserFile>

4b260f9bf99482302ea4a1f1de5b5a8c849ecff7
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-06-13 19:29:06 +0900

    backup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.serialization)
     alias(libs.plugins.parcelize)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.koin.compiler)
 }
 
 // Local properties

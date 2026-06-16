a4f69a0fa5e707a2e1ba86ad75f4677e82c85d20
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-06-16 13:09:39 +0900

    shape of search bar

--- app/src/main/java/com/mskd/flux/ui/component/global/FluxSearchField.kt
+++ app/src/main/java/com/mskd/flux/ui/component/global/FluxSearchField.kt
@@ -1,23 +1,28 @@
 package com.mskd.flux.ui.component.global
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.mskd.flux.R
-import com.mskd.flux.screens.search.SearchIntent
 
 @Composable
 fun FluxSearchField(
@@ -26,12 +31,24 @@ fun FluxSearchField(
     onValueChange: (String) -> Unit,
 ) {
 
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val cornerSize by animateDpAsState(
+        targetValue = if (isFocused) 32.dp else 16.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessMediumLow
+        ),
+        label = "TextFieldShapeAnimation"
+    )
+
     TextField(
         modifier = modifier,
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        shape = MaterialTheme.shapes.extraLargeIncreased,
+        shape = RoundedCornerShape(cornerSize),
         colors = TextFieldDefaults.colors(
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
@@ -46,7 +63,8 @@ fun FluxSearchField(
                     content = { Icon(imageVector = Icons.Rounded.Clear, contentDescription = "clear button") }
                 )
             }
-        }
+        },
+        interactionSource = interactionSource
     )
 
 }

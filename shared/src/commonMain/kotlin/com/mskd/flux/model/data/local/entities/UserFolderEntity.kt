bd3a67b08c2a85564bc85e05a468c3dbdbee1949
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-06-30 13:46:51 +0900

    backup

--- shared/src/commonMain/kotlin/com/mskd/flux/model/data/local/entities/UserFolderEntity.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/model/data/local/entities/UserFolderEntity.kt
@@ -1,7 +1,11 @@
 package com.mskd.flux.model.data.local.entities
 
+import androidx.room.Entity
 import com.mskd.flux.model.domain.FileSource
 
+@Entity(
+    tableName = "folders"
+)
 data class UserFolderEntity(
     val path: String,
     val source: FileSource

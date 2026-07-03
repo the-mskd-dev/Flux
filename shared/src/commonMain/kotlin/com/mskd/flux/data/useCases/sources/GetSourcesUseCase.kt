4d33e0c41849a998f0ddbff5ed51b52e7ebfc319
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-03 13:05:04 +0900

    GetSourcesUseCase

---
+++ shared/src/commonMain/kotlin/com/mskd/flux/data/useCases/sources/GetSourcesUseCase.kt
@@ -0,0 +1,23 @@
+package com.mskd.flux.data.useCases.sources
+
+import com.mskd.flux.data.repository.ddb.sources.SourcesRepository
+import com.mskd.flux.model.domain.files.FileSource
+import com.mskd.flux.model.domain.files.UserFolder
+
+class GetSourcesUseCase(
+    val repository: SourcesRepository,
+    val checkFolderUseCase: CheckFolderAvailabilityUseCase
+) {
+
+    suspend operator fun invoke() : List<UserFolder> {
+        return repository.getFolders().map { folder ->
+            if (folder.source == FileSource.LOCAL) {
+                val currentStatus = checkFolderUseCase(folder.path)
+                folder.copy(status = currentStatus)
+            } else {
+                folder
+            }
+        }
+    }
+
+}

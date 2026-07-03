a8eed8fb635e011623d71ee5aa7ecce5c7e0c9f3
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-04 00:20:18 +0900

    files feature

--- shared/src/androidMain/kotlin/com/mskd/flux/di/moduleRepository.android.kt
+++ shared/src/androidMain/kotlin/com/mskd/flux/di/moduleRepository.android.kt
@@ -2,32 +2,12 @@ package com.mskd.flux.di
 
 import com.mskd.flux.data.repository.connectivity.ConnectivityRepository
 import com.mskd.flux.data.repository.connectivity.ConnectivityRepositoryImpl
-import com.mskd.flux.data.repository.sources.SourcesFilesRepository
-import com.mskd.flux.features.files.data.MediaStoreFilesRepository
-import com.mskd.flux.features.files.data.SafFilesRepository
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val moduleRepositoryAndroid = module {
 
     single<ConnectivityRepository> {
         ConnectivityRepositoryImpl(context = get())
-    }
-
-    single<SourcesFilesRepository>(QualifiersAndroid.MEDIASTORE_SOURCES) {
-        MediaStoreFilesRepository(
-            context = get(),
-            userRepository = get()
-        )
-    }
-
-    singleOf(::SafVideoFilesDataSource)
-
-    single<SourcesFilesRepository>(QualifiersAndroid.SAF_SOURCES) {
-        SafFilesRepository(
-            sourcesRepository = get(),
-            safVideoFilesDataSource = get()
-        )
     }
 
 }

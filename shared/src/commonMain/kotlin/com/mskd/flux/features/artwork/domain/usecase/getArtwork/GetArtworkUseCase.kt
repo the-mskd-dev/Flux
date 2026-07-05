66f5dec91b99fa14c974044ef1560a929b6e7f1d
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-05 15:18:57 +0900

    delete useless

--- shared/src/commonMain/kotlin/com/mskd/flux/features/artwork/domain/usecase/getArtwork/GetArtworkUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.mskd.flux.features.artwork.domain.usecase.getArtwork
-
-import com.mskd.flux.core.domain.model.artwork.FullArtwork
-
-interface GetArtworkUseCase {
-    suspend operator fun invoke(artworkId: Long): FullArtwork?
-}

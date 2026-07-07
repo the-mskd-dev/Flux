6248ec3196f29601477709392cad1cace828570f
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-07 10:00:47 +0900

    CleanCatalogUseCaseImpl

--- shared/src/commonMain/kotlin/com/mskd/flux/features/catalog/domain/usecase/cleanCatalog/CleanCatalogUseCaseImpl.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/features/catalog/domain/usecase/cleanCatalog/CleanCatalogUseCaseImpl.kt
@@ -1,6 +1,7 @@
 package com.mskd.flux.features.catalog.domain.usecase.cleanCatalog
 
 import com.mskd.flux.core.data.database.repository.DatabaseRepository
+import com.mskd.flux.core.domain.model.files.FileSource
 import com.mskd.flux.features.files.domain.usecase.GetDeviceFilesUseCase
 import com.mskd.flux.features.sources.domain.model.UserFolder
 import com.mskd.flux.features.sources.domain.repository.SourcesRepository
@@ -13,16 +14,16 @@ internal class CleanCatalogUseCaseImpl(
     override suspend fun invoke() {
 
         val deviceFiles = getDeviceFilesUseCase()
-        /*val folders = sourcesRepository.getFolders()
 
-        val (accessibleFolders, inaccessibleFolders) = folders.partition { folder ->
-            folder.status == UserFolder.Status.AVAILABLE
-        }*/
+        val accessibleFolders = sourcesRepository
+            .getFolders()
+            .filter { it.status == UserFolder.Status.AVAILABLE }
+
+        val inaccessibleFiles = deviceFiles
+            .filter { file -> file.source == FileSource.SAF && accessibleFolders.none { file.path.startsWith(it.path) } }
+            .toSet()
 
 
-
-
-
-        database.deleteMediasNotInFiles(deviceFiles)
+        database.deleteMediasNotInFiles(deviceFiles - inaccessibleFiles)
     }
 }

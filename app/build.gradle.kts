5a56ef20d839f28a752af59def91321cb5a1e38b
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-08 14:06:02 +0900

    remove klint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.parcelize)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.klint)
 }
 
 // Local properties
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.koin.compiler)
-    alias(libs.plugins.klint)
 }
 
 kotlin {

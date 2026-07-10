60571d4f8a9eba9ea7f15a9eaa2ed6086e3732bf
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-11 01:17:23 +0900

    DeleteUnavailableSourcesUseCase backup

--- app/src/test/java/com/mskd/flux/features/sources/fake/FakeUserFolderValidator.kt
+++ app/src/test/java/com/mskd/flux/features/sources/fake/FakeUserFolderValidator.kt
@@ -3,10 +3,10 @@ package com.mskd.flux.features.sources.fake
 import com.mskd.flux.features.sources.domain.model.UserFolder
 import com.mskd.flux.features.sources.domain.validator.UserFolderValidator
 
-class FakeUserFolderValidator : UserFolderValidator {
+class FakeUserFolderValidator(private val returnValue: UserFolder.Status = UserFolder.Status.AVAILABLE) : UserFolderValidator {
 
     override suspend fun isFolderAvailable(path: String): UserFolder.Status {
-        return UserFolder.Status.AVAILABLE
+        return returnValue
     }
 
 }
---
+++ app/src/test/java/com/mskd/flux/features/sources/usecase/DeleteUnavailableSourcesUseCaseTest.kt
@@ -0,0 +1,7 @@
+package com.mskd.flux.features.sources.usecase
+
+import io.kotest.core.spec.style.FunSpec
+
+class DeleteUnavailableSourcesUseCaseTest : FunSpec({
+
+})

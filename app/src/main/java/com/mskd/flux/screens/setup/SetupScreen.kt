ce3da06fbcf80ef048dd0f65d2b23f4b758b77d6
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-30 12:14:59 +0900

    animation SetupScreen

--- app/src/main/java/com/mskd/flux/screens/setup/SetupScreen.kt
+++ app/src/main/java/com/mskd/flux/screens/setup/SetupScreen.kt
@@ -1,9 +1,12 @@
 package com.mskd.flux.screens.setup
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.widthIn
@@ -78,57 +81,55 @@ fun SetupScreenContent(
     sendIntent: (SetupIntent) -> Unit
 ) {
 
-    FluxScaffold(
-        title = null,
-        onBackTap = null,
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                onClick = { sendIntent(SetupIntent.OnNextButton) }
-            ) {
-                Text.Label.Large("Next")
-            }
-        }
-    ) { _ ->
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Image(
+            modifier = Modifier
+                .sizeIn(maxWidth = 600.dp, maxHeight = 600.dp)
+                .fillMaxSize()
+                .alpha(.15f),
+            painter = painterResource(Res.drawable.ic_flux),
+            contentDescription = "logo",
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+        )
 
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            modifier = Modifier
+                .padding(FluxUI.Space.medium)
+                .widthIn(max = 600.dp),
         ) {
 
-            Image(
-                modifier = Modifier
-                    .sizeIn(maxWidth = 600.dp, maxHeight = 600.dp)
-                    .fillMaxSize()
-                    .alpha(.15f),
-                painter = painterResource(Res.drawable.ic_flux),
-                contentDescription = "logo",
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-            )
+            AnimatedContent(
+                targetState = state.screen
+            ) { screen ->
 
-            Box(
-                modifier = Modifier
-                    .padding(FluxUI.Space.medium)
-                    .widthIn(max = 600.dp),
-            ) {
-
-                Crossfade(
-                    targetState = state.screen
-                ) { screen ->
-
-                    when (screen) {
-                        SetupContrat.Screen.WELCOME -> SetupWelcomeContent()
-                        SetupContrat.Screen.SOURCES -> SetupSourcesContent(
-                            selectedOption = state.sourcesOption,
-                            sendIntent = sendIntent
-                        )
-                    }
-
+                when (screen) {
+                    SetupContrat.Screen.WELCOME -> SetupWelcomeContent()
+                    SetupContrat.Screen.SOURCES -> SetupSourcesContent(
+                        selectedOption = state.sourcesOption,
+                        sendIntent = sendIntent
+                    )
                 }
 
             }
 
         }
 
+        ExtendedFloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(all = FluxUI.Space.medium)
+                .navigationBarsPadding()
+            ,
+            onClick = { sendIntent(SetupIntent.OnNextButton) }
+        ) {
+            Text.Label.Large("Next")
+        }
 
     }
 

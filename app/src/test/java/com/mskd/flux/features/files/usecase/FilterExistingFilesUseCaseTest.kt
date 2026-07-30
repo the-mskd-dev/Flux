a12f72d28e5f246ef640a2c38cb94f429b4edfb0
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-30 23:10:56 +0900

    FilterExistingFilesUseCaseTest & GetDeviceFilesUseCaseTest

--- app/src/test/java/com/mskd/flux/features/files/usecase/FilterExistingFilesUseCaseTest.kt
+++ app/src/test/java/com/mskd/flux/features/files/usecase/FilterExistingFilesUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.mskd.flux.features.files.usecase
 
 import com.mskd.flux.features.files.domain.usecase.FilterExistingFilesUseCaseImpl
 import com.mskd.flux.features.files.fake.FakeFilesDataSource
+import com.mskd.flux.features.sources.domain.provider.SourcesProvider
 import com.mskd.flux.mockups.FilesMockups
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
@@ -9,8 +10,18 @@ import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.subsequence
 import io.kotest.property.checkAll
+import io.mockk.coEvery
+import io.mockk.mockk
 
 class FilterExistingFilesUseCaseTest: FunSpec ({
+
+    lateinit var sourcesProvider: SourcesProvider
+
+    beforeTest {
+
+        sourcesProvider = mockk(relaxed = true)
+
+    }
 
     test("result should be the union between mediastore and saf") {
 
@@ -21,13 +32,9 @@ class FilterExistingFilesUseCaseTest: FunSpec ({
 
             val mediaStoreDataSource = FakeFilesDataSource(availableFiles = mediaStoreFiles)
             val safDataSource = FakeFilesDataSource(availableFiles = safFiles)
+            coEvery { sourcesProvider.getSources() } returns listOf(mediaStoreDataSource, safDataSource)
 
-            val useCase = FilterExistingFilesUseCaseImpl(
-                listOf(
-                    mediaStoreDataSource,
-                    safDataSource
-                )
-            )
+            val useCase = FilterExistingFilesUseCaseImpl(sourcesProvider = sourcesProvider)
 
             val inputFiles = FilesMockups.mediaStoreFiles + FilesMockups.safFiles
             val result = useCase(files = inputFiles)
@@ -42,13 +49,9 @@ class FilterExistingFilesUseCaseTest: FunSpec ({
     test("no file, no result") {
         val mediaStoreDataSource = FakeFilesDataSource(availableFiles = emptyList())
         val safDataSource = FakeFilesDataSource(availableFiles = emptyList())
+        coEvery { sourcesProvider.getSources() } returns listOf(mediaStoreDataSource, safDataSource)
 
-        val useCase = FilterExistingFilesUseCaseImpl(
-            listOf(
-                mediaStoreDataSource,
-                safDataSource
-            )
-        )
+        val useCase = FilterExistingFilesUseCaseImpl(sourcesProvider = sourcesProvider)
 
         val result = useCase(files = emptyList())
 

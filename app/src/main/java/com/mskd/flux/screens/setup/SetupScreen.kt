33f86920c6881878902c62e52cb2e99e63bdd092
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-30 12:04:41 +0900

    SetupScreen

--- app/src/main/java/com/mskd/flux/screens/setup/SetupScreen.kt
+++ app/src/main/java/com/mskd/flux/screens/setup/SetupScreen.kt
@@ -1,6 +1,8 @@
 package com.mskd.flux.screens.setup
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,33 +11,83 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.mskd.flux.features.setup.presentation.SetupContrat
+import com.mskd.flux.features.setup.presentation.SetupEvent
+import com.mskd.flux.features.setup.presentation.SetupIntent
+import com.mskd.flux.features.setup.presentation.SetupUiState
+import com.mskd.flux.features.setup.presentation.SetupViewModel
+import com.mskd.flux.features.sources.presentation.SourcesViewModel
+import com.mskd.flux.features.welcome.presentation.WelcomeEvent
+import com.mskd.flux.features.welcome.presentation.WelcomeIntent
+import com.mskd.flux.navigation.Route
+import com.mskd.flux.navigation.Route.*
+import com.mskd.flux.screens.setup.composables.SetupSourcesContent
 import com.mskd.flux.screens.setup.composables.SetupWelcomeContent
 import com.mskd.flux.ui.component.global.FluxScaffold
 import com.mskd.flux.ui.component.global.Text
 import com.mskd.flux.ui.theme.FluxUI
+import com.mskd.flux.utils.FluxPreview
+import com.mskd.flux.utils.FluxThemePreview
+import com.mskd.flux.utils.storagePermissionState
 import flux.shared.generated.resources.Res
 import flux.shared.generated.resources.ic_flux
 import org.jetbrains.compose.resources.painterResource
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.parameter.parametersOf
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun SetupScreen() {
+fun SetupScreen(
+    navigate: (Route) -> Unit,
+    viewModel: SetupViewModel = koinViewModel()
+) {
+
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+    val permissions = storagePermissionState()
+
+    LaunchedEffect(Unit) {
+        viewModel.event.collect { event ->
+            when (event) {
+                SetupEvent.NavigateToSources -> navigate(Sources(fromSetup = true))
+                SetupEvent.NavigateToToken -> navigate(Token(fromSetup = true))
+                SetupEvent.ShowPermissionDialog -> permissions.launchPermissionRequest()
+            }
+        }
+    }
+
+    if (permissions.status.isGranted) {
+        viewModel.handleIntent(SetupIntent.OnPermissionGranted)
+    }
+
+    SetupScreenContent(
+        state = state,
+        sendIntent = { viewModel.handleIntent(it) }
+    )
 
 }
 
 @Composable
-fun SetupScreenContent() {
+fun SetupScreenContent(
+    state: SetupUiState,
+    sendIntent: (SetupIntent) -> Unit
+) {
 
     FluxScaffold(
         title = null,
         onBackTap = null,
         floatingActionButton = {
             ExtendedFloatingActionButton(
-                onClick = {}
+                onClick = { sendIntent(SetupIntent.OnNextButton) }
             ) {
                 Text.Label.Large("Next")
             }
@@ -63,7 +115,19 @@ fun SetupScreenContent() {
                     .widthIn(max = 600.dp),
             ) {
 
-                SetupWelcomeContent()
+                Crossfade(
+                    targetState = state.screen
+                ) { screen ->
+
+                    when (screen) {
+                        SetupContrat.Screen.WELCOME -> SetupWelcomeContent()
+                        SetupContrat.Screen.SOURCES -> SetupSourcesContent(
+                            selectedOption = state.sourcesOption,
+                            sendIntent = sendIntent
+                        )
+                    }
+
+                }
 
             }
 
@@ -72,4 +136,30 @@ fun SetupScreenContent() {
 
     }
 
+}
+
+@FluxPreview
+@Composable
+fun SetupScreenContent_Welcome_Preview() {
+    FluxThemePreview {
+        SetupScreenContent(
+            state = SetupUiState(
+                screen = SetupContrat.Screen.WELCOME
+            ),
+            sendIntent = {}
+        )
+    }
+}
+
+@FluxPreview
+@Composable
+fun SetupScreenContent_Sources_Preview() {
+    FluxThemePreview {
+        SetupScreenContent(
+            state = SetupUiState(
+                screen = SetupContrat.Screen.SOURCES
+            ),
+            sendIntent = {}
+        )
+    }
 }

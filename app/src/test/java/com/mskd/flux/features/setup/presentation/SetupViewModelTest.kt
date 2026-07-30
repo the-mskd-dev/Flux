5c58390d9ef8c751bb7ac454cab6592551df7e74
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-30 13:37:49 +0900

    SetupViewModelTest

---
+++ app/src/test/java/com/mskd/flux/features/setup/presentation/SetupViewModelTest.kt
@@ -0,0 +1,106 @@
+package com.mskd.flux.features.setup.presentation
+
+import app.cash.turbine.test
+import com.mskd.flux.configs.fluxExtensions
+import com.mskd.flux.features.settings.domain.datastore.SettingsDataStore
+import com.mskd.flux.features.setup.domain.model.SetupScreen
+import com.mskd.flux.features.setup.domain.model.SourceSelectionMode
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coVerify
+import io.mockk.mockk
+
+class SetupViewModelTest : FunSpec( {
+
+    fluxExtensions()
+
+    lateinit var settingsDataStore: SettingsDataStore
+    lateinit var viewModel: SetupViewModel
+
+    beforeTest {
+
+        settingsDataStore = mockk(relaxed = true)
+
+        viewModel = SetupViewModel(settingsDataStore = settingsDataStore)
+
+    }
+
+    test("onNextButton - WELCOME - navigate to SOURCES") {
+        viewModel.uiState.test {
+
+            // Given
+            awaitItem()
+
+            // When
+            viewModel.handleIntent(SetupIntent.OnNextButton)
+
+            // Then
+            val screen = awaitItem().screen
+            screen shouldBe SetupScreen.SOURCES
+
+        }
+    }
+
+    test("onNextButton - SOURCES - if mode is DEFAULT, sent permissions event") {
+        // Given : go to SOURCES screen
+        viewModel.handleIntent(SetupIntent.OnNextButton)
+
+        viewModel.event.test {
+
+            // When
+            viewModel.handleIntent(SetupIntent.OnNextButton)
+
+            // Then
+            awaitItem() shouldBe SetupEvent.ShowPermissionDialog
+
+        }
+    }
+
+    test("onNextButton - SOURCES - if mode is CUSTOM, navigate to Token") {
+        // Given : go to SOURCES screen, then change mode to CUSTOM
+        viewModel.handleIntent(SetupIntent.OnNextButton)
+        viewModel.handleIntent(SetupIntent.SelectSourceSelectionMode(SourceSelectionMode.CUSTOM))
+
+        viewModel.event.test {
+
+            // When
+            viewModel.handleIntent(SetupIntent.OnNextButton)
+
+            // Then
+            awaitItem() shouldBe SetupEvent.NavigateToToken
+
+        }
+    }
+
+    test("selectSourceSelectionMode - change mode in SettingsDatastore") {
+        viewModel.uiState.test {
+
+            // Given
+            awaitItem()
+
+            // When
+            viewModel.handleIntent(SetupIntent.SelectSourceSelectionMode(SourceSelectionMode.CUSTOM))
+
+            // Then
+            awaitItem().sourceSelectionMode shouldBe SourceSelectionMode.CUSTOM
+
+            coVerify(exactly = 1) {
+                settingsDataStore.setSourceSelectionMode(mode = SourceSelectionMode.CUSTOM)
+            }
+
+        }
+    }
+
+    test("onPermissionGranted - navigate to TokenScreen") {
+        viewModel.event.test {
+
+            // When
+            viewModel.handleIntent(SetupIntent.OnPermissionGranted)
+
+            // Then
+            awaitItem() shouldBe SetupEvent.NavigateToToken
+
+        }
+    }
+
+})

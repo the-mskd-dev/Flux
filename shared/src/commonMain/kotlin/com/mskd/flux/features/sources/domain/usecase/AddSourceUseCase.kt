2c84d27ff15793fafe5416e3ebfe3b3b51639ec4
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-07-31 23:45:25 +0900

    fix AddSourceUseCase

--- shared/src/commonMain/kotlin/com/mskd/flux/features/sources/domain/usecase/AddSourceUseCase.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/features/sources/domain/usecase/AddSourceUseCase.kt
@@ -2,6 +2,8 @@ package com.mskd.flux.features.sources.domain.usecase
 
 import com.mskd.flux.features.sources.domain.model.UserFolder
 import com.mskd.flux.features.sources.domain.repository.SourcesRepository
+import io.ktor.http.decodeURLPart
+import io.ktor.http.parseUrl
 
 class AddSourceUseCase(
     val repository: SourcesRepository
@@ -13,7 +15,8 @@ class AddSourceUseCase(
         val newFolderAlreadyExists = folders.any { folder.path == it.path || folder.path.startsWith("${it.path}/") }
         if (newFolderAlreadyExists) return false
 
-        val alreadyIncludedFolders = folders.filter { it.path.startsWith("${folder.path}/") }
+        val decodePath = folder.path.decodeURLPart()
+        val alreadyIncludedFolders = folders.filter { it.path.decodeURLPart().startsWith("$decodePath/") }
         if (alreadyIncludedFolders.isNotEmpty()) {
             repository.deleteFolders(folders = alreadyIncludedFolders, deleteMedias = false)
         }

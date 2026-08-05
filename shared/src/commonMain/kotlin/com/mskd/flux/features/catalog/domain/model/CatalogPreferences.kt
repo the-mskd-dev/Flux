2b99d81345df8333492c9f53d130584b3f92b05e
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-05 13:55:11 +0900

    simplify code

--- shared/src/commonMain/kotlin/com/mskd/flux/features/catalog/domain/model/CatalogPreferences.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/features/catalog/domain/model/CatalogPreferences.kt
@@ -1,8 +1,10 @@
 package com.mskd.flux.features.catalog.domain.model
 
+import androidx.compose.runtime.Immutable
 import com.mskd.flux.core.datastore.domain.UserDataStore
 import com.mskd.flux.features.catalog.domain.datastore.CatalogDataStore
 
+@Immutable
 data class CatalogPreferences(
     val recentlyWatchedIds: List<Long>,
     val sortingOption: CatalogSortingOption,

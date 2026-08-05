c3d1cd161d7610848f60f3e2c4c26eaa917ad6f0
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-05 13:11:34 +0900

    CatalogChips

--- app/src/main/java/com/mskd/flux/screens/catalog/composable/CatalogChips.kt
+++ app/src/main/java/com/mskd/flux/screens/catalog/composable/CatalogChips.kt
@@ -5,14 +5,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.ButtonGroupDefaults
+import androidx.compose.material3.ChipColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.mskd.flux.features.catalog.domain.model.CatalogSorting
 import com.mskd.flux.features.catalog.domain.model.CatalogSortingOption
 import com.mskd.flux.features.catalog.presentation.CatalogIntent
 import com.mskd.flux.ui.component.global.Text
 import com.mskd.flux.ui.theme.FluxUI
+import com.mskd.flux.utils.FluxThemePreview
 import com.mskd.flux.utils.extensions.resolve
 
 @Composable
@@ -29,9 +35,19 @@ fun CatalogChips(
         AssistChip(
             onClick = { sendIntent(CatalogIntent.ShowSortingOptions(show = true)) },
             label = { Text.Button.Chip(sortingOption.description.resolve()) },
-            shape = CircleShape
+            shape = CircleShape,
         )
 
     }
 
+}
+
+@Preview
+@Composable
+fun CatalogChips_Preview() {
+    FluxThemePreview {
+        CatalogChips(
+            sortingOption = CatalogSortingOption.LAST_MODIFICATION
+        ) { }
+    }
 }

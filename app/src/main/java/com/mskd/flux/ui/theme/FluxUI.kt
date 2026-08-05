ccad7d56716422c5c7c0868234bf3dac900173e5
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-05 09:43:02 +0900

    FluxUI.Space.bottomScreen

--- app/src/main/java/com/mskd/flux/ui/theme/FluxUI.kt
+++ app/src/main/java/com/mskd/flux/ui/theme/FluxUI.kt
@@ -41,7 +41,7 @@ object FluxUI {
         val small = 8.dp
         val medium = 16.dp
         val large = 24.dp
-        val bottomScreen = 200.dp
+        val bottomScreen = 100.dp
     }
 
     object Animation {

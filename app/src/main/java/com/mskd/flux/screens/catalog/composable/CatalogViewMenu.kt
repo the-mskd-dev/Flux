4713435fd84fc311880a2f72b253edfed07698b3
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-07 00:08:49 +0900

    CatalogViewMenu position

--- app/src/main/java/com/mskd/flux/screens/catalog/composable/CatalogViewMenu.kt
+++ app/src/main/java/com/mskd/flux/screens/catalog/composable/CatalogViewMenu.kt
@@ -3,6 +3,7 @@ package com.mskd.flux.screens.catalog.composable
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AssistChip
@@ -11,6 +12,7 @@ import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mskd.flux.features.catalog.domain.model.CatalogSortingMode
@@ -36,8 +38,10 @@ fun CatalogViewMenu(
 ) {
 
     Row(
-        modifier = Modifier.padding(horizontal = FluxUI.Space.medium),
-        horizontalArrangement = Arrangement.spacedBy(FluxUI.Space.extraSmall)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = FluxUI.Space.medium),
+        horizontalArrangement = Arrangement.spacedBy(FluxUI.Space.extraSmall, Alignment.End),
     ) {
 
         FilledTonalButton(

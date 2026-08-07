1d24b06b231830bee140b4d71519ce3ce29c186f
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-08 00:14:09 +0900

    GenreDto

---
+++ shared/src/commonMain/kotlin/com/mskd/flux/core/model/artwork/Genre.kt
@@ -0,0 +1,6 @@
+package com.mskd.flux.core.model.artwork
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ shared/src/commonMain/kotlin/com/mskd/flux/core/network/tmdb/data/dto/GenreDto.kt
@@ -0,0 +1,6 @@
+package com.mskd.flux.core.network.tmdb.data.dto
+
+data class GenreDto(
+    val id: Int,
+    val name: String
+)

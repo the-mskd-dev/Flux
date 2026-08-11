36f162f7d347a15bfbae91338b105d0223efcd70
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-11 09:43:25 +0900

    backup

--- shared/src/commonMain/kotlin/com/mskd/flux/features/catalog/domain/usecase/syncGenres/UpdateGenreIdsUseCase.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/features/catalog/domain/usecase/syncGenres/UpdateGenreIdsUseCase.kt
@@ -16,9 +16,6 @@ class UpdateGenreIdsUseCase(
 
         val artworks = db.getArtworks().filter { it.genreIds.isEmpty() && !it.isUnknown }
 
-        if (artworks.isEmpty())
-            return
-
         onProgressCount(artworks.size)
 
         val updatedArtworks = coroutineScope {

a2f0c7aaca0eca72429f11a9719d06a2ae02e281
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-14 00:03:01 +0900

    bottom space

--- app/src/main/java/com/mskd/flux/screens/catalog/CatalogScreen.kt
+++ app/src/main/java/com/mskd/flux/screens/catalog/CatalogScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
--- app/src/main/java/com/mskd/flux/ui/theme/FluxUI.kt
+++ app/src/main/java/com/mskd/flux/ui/theme/FluxUI.kt
@@ -43,7 +43,7 @@ object FluxUI {
         val small = 8.dp
         val medium = 16.dp
         val large = 24.dp
-        val bottomScreen = 100.dp
+        val bottomScreen = 150.dp
     }
 
     object Animation {

5ce5dcde38065518a56a412d644d8c967701201f
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-16 19:09:58 +0900

    [UPDATE] Version 1.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ configure<ApplicationExtension> {
         applicationId = "com.mskd.flux"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 32
-        versionName = "1.7.1"
+        versionCode = 33
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }

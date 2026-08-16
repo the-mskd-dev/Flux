d4174a3c305d64c1643d983a8959763185ff327a
the masked dev <the.masked.dev@proton.me>
2026-08-16 18:57:23 +0900

    [CORE] Improve image loading (#326)
    * multi ImageLoader
    
    * smaller SD image

--- shared/src/androidMain/kotlin/com/mskd/flux/features/images/moduleImagesAndroid.kt
+++ shared/src/androidMain/kotlin/com/mskd/flux/features/images/moduleImagesAndroid.kt
@@ -1,40 +1,80 @@
 package com.mskd.flux.features.images
 
 import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
 import coil3.disk.DiskCache
 import coil3.disk.directory
 import coil3.memory.MemoryCache
+import coil3.network.ktor3.KtorNetworkFetcherFactory
 import coil3.video.VideoFrameDecoder
 import com.mskd.flux.features.images.data.AndroidImageRequestFactory
 import com.mskd.flux.features.images.domain.ImageRequestFactory
 import com.mskd.flux.features.images.domain.NetworkImageInterceptor
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import okhttp3.Dispatcher
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
+@OptIn(ExperimentalCoilApi::class)
 val moduleImagesAndroid = module {
 
     singleOf(::NetworkImageInterceptor)
 
-    single<ImageLoader> {
-        val context = androidContext()
-        val networkImageInterceptor = get<NetworkImageInterceptor>()
+    single<MemoryCache> {
+        MemoryCache.Builder()
+            .maxSizePercent(androidContext(), 0.25)
+            .build()
+    }
 
-        ImageLoader.Builder(context)
-            .memoryCache {
-                MemoryCache.Builder()
-                    .maxSizePercent(context, 0.25)
-                    .build()
+    single<DiskCache> {
+        DiskCache.Builder()
+            .directory(androidContext().filesDir.resolve("image_cache"))
+            .maxSizeBytes(512L * 1024L * 1024L)
+            .build()
+    }
+
+    single(named("uiClient")) {
+        HttpClient(OkHttp) {
+            engine {
+                config {
+                    dispatcher(Dispatcher().apply { maxRequestsPerHost = 6 })
+                }
             }
-            .diskCache {
-                DiskCache.Builder()
-                    .directory(context.filesDir.resolve("image_cache"))
-                    .maxSizeBytes(512L * 1024L * 1024L)
-                    .build()
+        }
+    }
+
+    single(named("cacheClient")) {
+        HttpClient(OkHttp) {
+            engine {
+                config {
+                    dispatcher(Dispatcher().apply { maxRequestsPerHost = 2 })
+                }
             }
+        }
+    }
+
+    single<ImageLoader>(named("uiImageLoader")) {
+        ImageLoader.Builder(androidContext())
+            .memoryCache { get<MemoryCache>() }
+            .diskCache { get<DiskCache>() }
             .components {
                 add(VideoFrameDecoder.Factory())
-                add(networkImageInterceptor)
+                add(get<NetworkImageInterceptor>())
+                add(KtorNetworkFetcherFactory(httpClient = get<HttpClient>(named("uiClient"))))
+            }
+            .build()
+    }
+
+    single<ImageLoader>(named("cacheImageLoader")) {
+        ImageLoader.Builder(androidContext())
+            .memoryCache { get<MemoryCache>() }
+            .diskCache { get<DiskCache>() }
+            .components {
+                add(get<NetworkImageInterceptor>())
+                add(KtorNetworkFetcherFactory(httpClient = get<HttpClient>(named("cacheClient"))))
             }
             .build()
     }
--- shared/src/commonMain/kotlin/com/mskd/flux/features/images/moduleImages.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/features/images/moduleImages.kt
@@ -3,6 +3,7 @@ package com.mskd.flux.features.images
 import com.mskd.flux.di.Qualifiers
 import com.mskd.flux.features.images.domain.ImagesPrefetchManager
 import com.mskd.flux.features.images.domain.ImagesPrefetchManagerImpl
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val moduleImages = module {
@@ -11,7 +12,7 @@ val moduleImages = module {
         ImagesPrefetchManagerImpl(
             database = get(),
             settings = get(),
-            imageLoader = get(),
+            imageLoader = get(named("cacheImageLoader")),
             imageRequestFactory = get(),
             scope = get(Qualifiers.APPLICATION_SCOPE)
         )

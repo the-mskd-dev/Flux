66c6e96f8eb8460d2eb2a8030467c8eac1ad2c34
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-08-23 12:51:28 +0200

    backup

--- shared/src/commonMain/kotlin/com/mskd/flux/features/history/domain/usecase/SaveToHistoryUseCase.kt
+++ shared/src/commonMain/kotlin/com/mskd/flux/features/history/domain/usecase/SaveToHistoryUseCase.kt
@@ -1,15 +1,26 @@
 package com.mskd.flux.features.history.domain.usecase
 
 import com.mskd.flux.core.database.domain.repository.DatabaseRepository
+import com.mskd.flux.core.model.artwork.Episode
 import com.mskd.flux.core.model.artwork.Media
+import com.mskd.flux.core.model.artwork.Movie
 import com.mskd.flux.features.history.domain.repository.HistoryRepository
 
 class SaveToHistoryUseCase(
-    private val repository: HistoryRepository,
-    private val artworkRepository: DatabaseRepository
+    private val history: HistoryRepository,
+    private val database: DatabaseRepository
 ) {
 
     suspend operator fun invoke(media: Media) {
+
+        when (media) {
+            is Episode -> {
+
+            }
+            is Movie -> {
+
+            }
+        }
 
     }
 

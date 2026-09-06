f14adda540afb40e12dd108ab9750a16cf29a549
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-09-06 18:57:03 +0200

    new ui for history

--- app/src/main/java/com/mskd/flux/screens/catalog/composable/CatalogHistory.kt
+++ app/src/main/java/com/mskd/flux/screens/catalog/composable/CatalogHistory.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,17 +12,23 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.mskd.flux.core.model.artwork.Episode
@@ -40,11 +47,14 @@ import com.mskd.flux.utils.FluxPreview
 import com.mskd.flux.utils.FluxThemePreview
 import com.mskd.flux.utils.extensions.bleedHorizontal
 import com.mskd.flux.utils.extensions.fillMaxWidthWithLimit
+import com.mskd.flux.utils.extensions.minToMs
+import com.mskd.flux.utils.extensions.timeDescription
 import flux.shared.generated.resources.Res
 import flux.shared.generated.resources.continue_watching
 import flux.shared.generated.resources.delete
 import flux.shared.generated.resources.episode
 import flux.shared.generated.resources.ic_delete
+import flux.shared.generated.resources.remaining_time
 import flux.shared.generated.resources.season
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -112,38 +122,33 @@ fun CatalogHistoryItem(
     val shape = LocalUiShapes.current.corners
 
     Card(
-        modifier = modifier
-            .fillMaxWidthWithLimit(
-                fraction = .8f,
-                max = 450.dp
-            )
-            .aspectRatio(FluxUI.Ratio.imax)
-            .combinedClickable(
-                onClick = { sendIntent(CatalogIntent.PlayMedia(media = media)) },
-                onLongClick = { showMenu = true }
-            ),
-        shape = shape
+        modifier = modifier.fillMaxWidthWithLimit(fraction = .8f, max = 450.dp),
+        shape = shape,
+        onClick = { sendIntent(CatalogIntent.PlayMedia(media = media)) }
     ) {
 
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(all = FluxUI.Space.small),
+            verticalArrangement = Arrangement.spacedBy(FluxUI.Space.small)
+        ) {
 
-            FluxImage(
+            Box(
                 modifier = Modifier
+                    .clip(shape)
                     .fillMaxWidth()
-                    .weight(.7f),
-                media = media,
-                contentDescription = media.title,
-                videoFrame = true
-            )
-
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(.3f)
-                    .background(color = MaterialTheme.colorScheme.surfaceContainer)
-                    .padding(vertical = FluxUI.Space.small, horizontal = FluxUI.Space.medium),
-                verticalArrangement = Arrangement.spacedBy(FluxUI.Space.extraSmall),
+                    .aspectRatio(FluxUI.Ratio.landscape),
+                contentAlignment = Alignment.BottomCenter
             ) {
+
+                FluxImage(
+                    modifier = Modifier.fillMaxSize(),
+                    media = media,
+                    contentDescription = media.title,
+                    videoFrame = true
+                )
 
                 ProgressStatusBar(
                     modifier = Modifier.fillMaxWidth(),
@@ -151,11 +156,18 @@ fun CatalogHistoryItem(
                     progress = { media.progressPercent },
                 )
 
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = FluxUI.Space.small),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
                 Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-                    verticalArrangement = Arrangement.Center
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(FluxUI.Space.extraSmall),
                 ) {
 
                     Text.Card.Title(
@@ -166,39 +178,59 @@ fun CatalogHistoryItem(
                         maxLines = 1
                     )
 
-                    (media as? Episode)?.let {
-                        Row(horizontalArrangement = Arrangement.spacedBy(FluxUI.Space.small)) {
-                            Text.Card.Label(
-                                text = "${stringResource(Res.string.season, it.season)}, ${stringResource(Res.string.episode, it.number)}",
-                                color = MaterialTheme.colorScheme.primary,
-                            )
+                    val remainingTime = (media.duration.minToMs - media.currentTime).timeDescription(withoutSeconds = true)
+                    val description = buildString {
+
+                        if (media is Episode) {
+                            append("S${media.season}:E${media.number}")
+                            append(" • ")
                         }
+
+                        append(stringResource(Res.string.remaining_time, remainingTime))
                     }
 
+                    Text.Card.Label(
+                        text = description,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
                 }
+
+                IconButton(
+                    onClick = { showMenu = true },
+                    content = {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "menu button"
+                        )
+
+                        if (showMenu) {
+                            FluxDropDownMenu(
+                                onDismissRequest = { showMenu = false },
+                                items = listOf(
+                                    FluxDropDownMenuItem(
+                                        text = stringResource(Res.string.delete),
+                                        onClick = { sendIntent(CatalogIntent.DeleteHistoryEntry(entry = entry)) },
+                                        leadingIcon = {
+                                            Icon(
+                                                painter = painterResource(Res.drawable.ic_delete),
+                                                contentDescription = stringResource(Res.string.delete)
+                                            )
+                                        },
+                                    )
+                                )
+                            )
+                        }
+
+                    }
+                )
 
             }
 
         }
 
-    }
-
-    if (showMenu) {
-        FluxDropDownMenu(
-            onDismissRequest = { showMenu = false },
-            items = listOf(
-                FluxDropDownMenuItem(
-                    text = stringResource(Res.string.delete),
-                    onClick = { sendIntent(CatalogIntent.DeleteHistoryEntry(entry = entry)) },
-                    leadingIcon = {
-                        Icon(
-                            painter = painterResource(Res.drawable.ic_delete),
-                            contentDescription = stringResource(Res.string.delete)
-                        )
-                    },
-                )
-            )
-        )
     }
 
 }
@@ -208,7 +240,10 @@ fun CatalogHistoryItem(
 fun CatalogHistoryItem_Preview() {
     FluxThemePreview {
         CatalogHistory(
-            entries = MediaMockups.episodes.map { it.toHistoryEntry() }
+            entries = MediaMockups
+                .allMedias
+                .distinctBy { it.artworkId }
+                .map { it.toHistoryEntry() }
         ) { }
     }
 }

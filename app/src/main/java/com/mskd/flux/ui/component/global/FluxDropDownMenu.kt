d61f51979d2550d9d662e9270e93c5436020ccde
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-09-06 19:34:15 +0200

    FluxDropDownMenu UI

--- app/src/main/java/com/mskd/flux/ui/component/global/FluxDropDownMenu.kt
+++ app/src/main/java/com/mskd/flux/ui/component/global/FluxDropDownMenu.kt
@@ -1,12 +1,19 @@
 package com.mskd.flux.ui.component.global
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FluxDropDownMenu(
@@ -15,24 +22,31 @@ fun FluxDropDownMenu(
 ) {
 
     DropdownMenu(
-        shape = MaterialTheme.shapes.extraLarge,
         expanded = true,
         onDismissRequest = onDismissRequest,
-        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+        containerColor = Color.Transparent,
+        shadowElevation = 0.dp,
         content = {
 
-            items.forEach { item ->
+            Column(
+                modifier = Modifier.clip(MaterialTheme.shapes.extraLarge),
+                verticalArrangement = Arrangement.spacedBy(2.dp)
+            ) {
 
-                DropdownMenuItem(
-                    modifier = Modifier.background(MaterialTheme.colorScheme.tertiaryContainer),
-                    colors = MenuDefaults.itemColors(
-                        textColor = MaterialTheme.colorScheme.onTertiaryContainer,
-                        leadingIconColor = MaterialTheme.colorScheme.onTertiaryContainer,
-                    ),
-                    onClick = item.onClick,
-                    text = { Text.Card.Body(text = item.text) },
-                    leadingIcon = item.leadingIcon,
-                )
+                items.forEach { item ->
+
+                    DropdownMenuItem(
+                        modifier = Modifier.background(MaterialTheme.colorScheme.tertiaryContainer),
+                        colors = MenuDefaults.itemColors(
+                            textColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                            leadingIconColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                        ),
+                        onClick = item.onClick,
+                        text = { Text.Card.Body(text = item.text) },
+                        leadingIcon = item.leadingIcon,
+                    )
+
+                }
 
             }
 

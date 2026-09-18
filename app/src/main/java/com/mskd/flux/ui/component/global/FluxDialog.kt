fffad5ceedad7867215f9e6b3ae6275892536de0
the-mskd-dev <kevyn.buchet.pro@gmail.com>
2026-09-19 00:06:15 +0200

    backup

--- app/src/main/java/com/mskd/flux/ui/component/global/FluxDialog.kt
+++ app/src/main/java/com/mskd/flux/ui/component/global/FluxDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -40,7 +41,7 @@ fun FluxDialog(
         onDismissRequest = onDismiss,
         properties = DialogProperties(decorFitsSystemWindows = false)
     ) {
-        Surface(
+        Card(
             shape = FluxUI.shapes.corners,
             modifier = Modifier.imePadding()
         ) {

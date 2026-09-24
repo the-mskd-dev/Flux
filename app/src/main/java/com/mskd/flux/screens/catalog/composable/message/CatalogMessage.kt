836fe57946ac300d9e959830b51c6e807d3f9380
mskd-pro <the.masked.dev@proton.me>
2026-09-24 15:09:49 +0200

    MessageScreen

--- app/src/main/java/com/mskd/flux/screens/catalog/composable/message/CatalogMessage.kt
+++ app/src/main/java/com/mskd/flux/screens/catalog/composable/message/CatalogMessage.kt
@@ -1,4 +1,4 @@
-package com.mskd.flux.screens.catalog.composable.message
+    package com.mskd.flux.screens.catalog.composable.message
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
